2b685ad4474232d223aace3c5b1968269317df3f
Ruth Monteiro <felicia.monteiro@ngc.com>
2017-07-10 13:40:42 -0400

    swagger integration

--- src/main/java/gov/nih/nci/evs/api/Application.java
+++ src/main/java/gov/nih/nci/evs/api/Application.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application extends SpringBootServletInitializer {
-
+//public class Application {
 	 @Override
 	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	        return application.sources(Application.class);
@@ -17,6 +25,16 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+	@Bean
+        public Docket api() { 
+               return new Docket(DocumentationType.SWAGGER_2)  
+                 .select()                                  
+                 .apis(RequestHandlerSelectors.any())              
+                 .paths(PathSelectors.ant("/api/v1/ctrp/concept/**"))                          
+                 .build();                                           
+        }
 
 	
 }
