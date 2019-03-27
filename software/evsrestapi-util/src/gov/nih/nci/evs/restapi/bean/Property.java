1241588f0a07ac6662ed68b7ba90a35b651bf2a9
Kim Ong <kim.ong@nih.gov>
2019-03-27 05:54:23 -0400

    Modified Property.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/bean/Property.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/bean/Property.java
@@ -6,6 +6,7 @@ import java.net.*;
 
 public class Property extends NameAndValue
 {
+	    private String source = null;
 	// Default constructor
 		public Property() {
 			super();
@@ -15,5 +16,20 @@ public class Property extends NameAndValue
 			String name,
 			String value) {
 			super(name, value);
+		}
+
+		public Property(
+			String name,
+			String value,
+			String source) {
+			super(name, value);
+		}
+
+		public void setSource(String source) {
+			this.source = source;
+		}
+
+		public String getSource() {
+			return this.source;
 		}
 }
