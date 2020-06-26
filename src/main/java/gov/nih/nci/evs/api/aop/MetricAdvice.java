be59739e93628a6ca5671450d7354802eeb7bd87
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2020-06-25 17:56:03 -0700

    elasticsearch change to metrics

--- src/main/java/gov/nih/nci/evs/api/aop/MetricAdvice.java
+++ src/main/java/gov/nih/nci/evs/api/aop/MetricAdvice.java
@@ -3,6 +3,7 @@ package gov.nih.nci.evs.api.aop;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,13 +13,19 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.nih.nci.evs.api.model.Metric;
+import gov.nih.nci.evs.api.properties.ElasticServerProperties;
 
 /**
  * Metric advice.
@@ -29,6 +36,9 @@ public class MetricAdvice {
 
   /** The logger. */
   private static final Logger log = LoggerFactory.getLogger(MetricAdvice.class);
+
+  @Autowired
+  ElasticServerProperties elasticServerProperties;
 
   /**
    * Record metric.
@@ -43,7 +53,6 @@ public class MetricAdvice {
   @Around("execution(* gov.nih.nci.evs.api.controller.*.*(..)) && @annotation(recordMetric)")
   private Object recordMetric(ProceedingJoinPoint pjp, RecordMetric recordMetric)
     throws Throwable {
-    log.info("log method having db as parameter");
 
     // get the request
     HttpServletRequest request =
@@ -55,6 +64,8 @@ public class MetricAdvice {
 
   }
 
+  // unchecked conversion doesn't matter unless 6.7 somehow changes the format of the response
+  @SuppressWarnings("unchecked")
   public Object recordMetricHelper(ProceedingJoinPoint pjp, HttpServletRequest request, Map<String, String[]> params)
     throws Throwable {
 
@@ -67,7 +78,6 @@ public class MetricAdvice {
     Date endDate = new Date();
     Metric metric = new Metric();
 
-    log.info("duration = " + String.valueOf(duration));
     metric.setDuration(duration);
 
     // get the ip address of the remote user
@@ -76,29 +86,32 @@ public class MetricAdvice {
 
     String userIpAddress = attr.getRequest().getRemoteAddr();
     metric.setRemoteIpAddress(userIpAddress);
-    log.info("userIpAddress = " + userIpAddress);
 
     String hostName = attr.getRequest().getRemoteHost();
     metric.setHostName(hostName);
-    log.info("hostName = " + hostName);
 
     String url = request.getRequestURL().toString();
     metric.setEndPoint(url);
-    log.info("url = " + url);
     
     metric.setQueryParams(params);
-    log.info("params = " + params);
-    
-
     metric.setStartTime(startDate);
     metric.setEndTime(endDate);
 
     // get the parameters
     ObjectMapper mapper = new ObjectMapper();
+    RestTemplate restTemplate = new RestTemplate();
     mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
     String metricStr = mapper.writeValueAsString(metric);
-    log.info("metric = " + metricStr);
 
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<String> metricData = new HttpEntity<String>(metricStr, headers);
+    String elasticSearchUrl = elasticServerProperties.getUrl();
+    String response = restTemplate.postForObject(elasticSearchUrl.replace("concept/_search","metrics/_doc/"), metricData, String.class);
+    Map<String, Object> map=new HashMap<String, Object>();
+    map = mapper.readValue(response, HashMap.class);
+    log.info("metrics object id = " + map.get("_id"));
+    log.info(restTemplate.getForObject(elasticSearchUrl.replace("concept/_search","metrics/_doc/") + map.get("_id"), String.class));
     return retVal;
   }
 
