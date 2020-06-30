218f495c2c0a992134d527c9c6bac797fcf8b6c1
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2020-06-30 23:51:02 +0100

    Add metadata properties

---
+++ src/main/java/gov/nih/nci/evs/api/properties/MetadataProperties.java
@@ -0,0 +1,13 @@
+
+package gov.nih.nci.evs.api.properties;
+
+import java.util.Map;
+
+/**
+ * Metadata properties for how to map from terminology specific structures to EVS model.
+ */
+public class MetadataProperties {
+
+
+
+}
