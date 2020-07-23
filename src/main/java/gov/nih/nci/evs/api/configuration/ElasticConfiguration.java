c29f033e6abfb50bc5d0d95f24e243c9bb176338
Arun Narasani <sfbayhacker@outlook.com>
2020-07-23 00:21:40 -0700

    using elasticsearch rest client

--- src/main/java/gov/nih/nci/evs/api/configuration/ElasticConfiguration.java
+++ src/main/java/gov/nih/nci/evs/api/configuration/ElasticConfiguration.java
@@ -1,19 +1,14 @@
 package gov.nih.nci.evs.api.configuration;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
 
 @Configuration
 public class ElasticConfiguration {
@@ -26,22 +21,12 @@ public class ElasticConfiguration {
   Environment env;
 
   @Bean
-  Client client() throws UnknownHostException {
-    if (logger.isDebugEnabled()) {
-      logger.debug("Configuring elasticsearch client");
-      logger.debug("    cluster name : {}", env.getProperty("nci.evs.elasticsearch.server.clusterName"));
-      logger.debug("    server host  : {}", env.getProperty("nci.evs.elasticsearch.server.host"));
-      logger.debug("    server port  : {}", env.getProperty("nci.evs.elasticsearch.server.port"));
-    }
-    
-    Settings settings = Settings.builder()
-        .put("http.enabled","true")
-        .put("client.transport.sniff", "false")
-        .put("cluster.name", env.getProperty("nci.evs.elasticsearch.server.clusterName")).build();
-    TransportClient client = new PreBuiltTransportClient(settings);
-    client.addTransportAddress(
-        new TransportAddress(InetAddress.getByName(env.getProperty("nci.evs.elasticsearch.server.host")),
-            Integer.parseInt(env.getProperty("nci.evs.elasticsearch.server.port"))));
-    return client;
+  RestHighLevelClient client() {
+    String esHost = env.getProperty("nci.evs.elasticsearch.server.host") + ":" + env.getProperty("nci.evs.elasticsearch.server.port");
+    logger.info(String.format("Configuring es client for host %s", esHost));
+    ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+        .connectedTo(esHost)
+        .build();
+    return RestClients.create(clientConfiguration).rest();
   }
 }
