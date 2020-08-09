98c21c10318425a0e5e203cc118916aa17c047a3
Arun Narasani <sfbayhacker@outlook.com>
2020-08-08 19:30:01 -0700

    query logging using custom ElasticsearchRestTemplate

--- src/main/java/gov/nih/nci/evs/api/configuration/ElasticConfiguration.java
+++ src/main/java/gov/nih/nci/evs/api/configuration/ElasticConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
 @Configuration
 public class ElasticConfiguration {
@@ -30,5 +31,10 @@ public class ElasticConfiguration {
     // ClientConfiguration clientConfiguration =
     // ClientConfiguration.builder().connectedTo(esHost)..build();
     // return RestClients.create(clientConfiguration).rest();
+  }
+  
+  @Bean(name = "elasticsearchTemplate")
+  ElasticsearchRestTemplate elasticRestTemplate() {
+    return new EVSElasticsearchRestTemplate(client());
   }
 }
