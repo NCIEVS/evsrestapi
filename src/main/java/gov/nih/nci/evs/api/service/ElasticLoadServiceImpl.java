1d6d6f9079696a6654a2ea3e1b9032450233c83f
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2020-09-22 13:46:46 -0700

    remove non-Real Time stuff from loadConcepts

--- src/main/java/gov/nih/nci/evs/api/service/ElasticLoadServiceImpl.java
+++ src/main/java/gov/nih/nci/evs/api/service/ElasticLoadServiceImpl.java
@@ -1,18 +1,7 @@
 
 package gov.nih.nci.evs.api.service;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
@@ -35,8 +24,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.nih.nci.evs.api.Application;
 import gov.nih.nci.evs.api.model.Concept;
@@ -115,167 +102,20 @@ public class ElasticLoadServiceImpl implements ElasticLoadService {
     logger.info("Getting all concepts");
     List<Concept> allConcepts = sparqlQueryManagerService.getAllConcepts(terminology);
 
-    if (config.isRealTime()) {
-      try {
-        // download concepts and upload to es in real time
-        logger.info("Loading in real time");
-        loadConceptsRealTime(allConcepts, terminology, hierarchy);
-      } catch (Exception e) {
-        logger.error(e.getMessage(), e);
-        throw new IOException(e);
-      }
+	try {
+	  // download concepts and upload to es in real time
+	  logger.info("Loading in real time");
+	  loadConceptsRealTime(allConcepts, terminology, hierarchy);
+	} catch (Exception e) {
+	  logger.error(e.getMessage(), e);
+	  throw new IOException(e);
+	}
+	
+	// compare count of concepts loaded and index metadata object
+	checkLoadStatusandIndexMetadata(allConcepts.size(), terminology);
+	
+	return;
 
-      // compare count of concepts loaded and index metadata object
-      checkLoadStatusandIndexMetadata(allConcepts.size(), terminology);
-
-      return;
-    }
-
-    // get all concepts
-    if (!config.isSkipDownload()) {
-      // download files from stardog in batches
-      downloadConcepts(allConcepts, terminology, config.getLocation(), hierarchy);
-    }
-
-    if (config.isDownloadOnly()) {
-      logger.info("DownloadOnly option is True. Skipping load process.");
-      return;
-    }
-
-    // load concepts to es in batches
-    try {
-      logger.info("Loading concepts from files");
-      loadConceptsFromFiles(config.getLocation(), terminology, hierarchy);
-    } catch (InterruptedException e) {
-      logger.error(e.getMessage(), e);
-      throw new IOException(e);
-    }
-
-    // compare count of concepts loaded and index metadata object
-    checkLoadStatusandIndexMetadata(allConcepts.size(), terminology);
-
-    return;
-  }
-
-  /**
-   * download concepts.
-   *
-   * @param allConcepts all concepts to download
-   * @param terminology the terminology
-   * @param location the location to download
-   * @param hierarchy the hierarchy
-   * @throws IOException the io exception
-   */
-  private void downloadConcepts(List<Concept> allConcepts, Terminology terminology, String location,
-    HierarchyUtils hierarchy) throws IOException {
-    if (CollectionUtils.isEmpty(allConcepts))
-      return;
-    logger.debug("Downloading concepts");
-    int start = 0;
-    int end = DOWNLOAD_BATCH_SIZE;
-
-    int total = allConcepts.size();
-    logger.info("  Total concepts to download: {}", total);
-
-    while (start < total) {
-      if (total - start <= DOWNLOAD_BATCH_SIZE)
-        end = total;
-
-      logger.info("  Processing {} to {}", start + 1, end);
-
-      List<Concept> concepts = sparqlQueryManagerService
-          .getConcepts(allConcepts.subList(start, end), terminology, hierarchy);
-
-      for (Concept concept : concepts) {
-        String json = concept.toString();
-        String filePath = location + concept.getCode() + ".json";
-        BufferedWriter writer = new BufferedWriter(
-            new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8));
-        writer.write(json);
-        writer.close();
-      }
-
-      start = end;
-      end = end + DOWNLOAD_BATCH_SIZE;
-    }
-
-    Files.createFile(FileSystems.getDefault().getPath(location, LOCK_FILE));
-    logger.info("Download process complete!");
-  }
-
-  /**
-   * load concepts from files in batches.
-   *
-   * @param location the location where files are to be read from
-   * @param terminology the terminology
-   * @param hierarchy the hierarchy
-   * @throws IOException the io exception
-   * @throws InterruptedException the interruped exception
-   */
-  private void loadConceptsFromFiles(String location, Terminology terminology,
-    HierarchyUtils hierarchy) throws IOException, InterruptedException {
-    logger.debug("Loading concepts from files");
-    File conceptsDir = new File(location);
-    File lockFile = new File(location + LOCK_FILE);
-
-    if (!conceptsDir.exists() || !lockFile.exists()) {
-      logger.warn("Unable to load concepts. Lock file is missing!");
-      return;
-    }
-
-    File[] jsonFiles = conceptsDir.listFiles(new FilenameFilter() {
-      public boolean accept(File dir, String name) {
-        return name.toLowerCase().endsWith(".json");
-      }
-    });
-
-    if (jsonFiles == null || jsonFiles.length == 0) {
-      logger.warn("No JSON files found to load!");
-      return;
-    }
-
-    logger.info("  Total concepts to load: {}", jsonFiles.length);
-
-    Double total = (double) jsonFiles.length;
-
-    int start = 0;
-    int end = INDEX_BATCH_SIZE;
-
-    Double taskSize = Math.ceil(total / INDEX_BATCH_SIZE);
-
-    logger.info("  Task size: {}", taskSize.intValue());
-
-    CountDownLatch latch = new CountDownLatch(taskSize.intValue());
-    ExecutorService executor = Executors.newFixedThreadPool(10);
-
-    while (start < total) {
-      if (total - start <= INDEX_BATCH_SIZE)
-        end = total.intValue();
-
-      logger.info("  Processing {} to {}", start + 1, end);
-
-      File[] files = Arrays.copyOfRange(jsonFiles, start, end);
-      List<Concept> concepts = new ArrayList<>(files.length);
-      ObjectMapper mapper = new ObjectMapper();
-      for (File file : files) {
-        byte[] data = Files.readAllBytes(Paths.get(file.toURI()));
-        String strData = new String(data, "UTF-8");
-        Concept concept = mapper.readValue(strData.getBytes("UTF-8"), Concept.class);
-        concepts.add(concept);
-      }
-
-      executor.submit(new ConceptLoadTask(concepts, start, end, terminology.getIndexName(), latch,
-          taskSize.intValue()));
-
-      start = end;
-      end = end + INDEX_BATCH_SIZE;
-    }
-
-    latch.await();
-
-    executor.shutdown();
-
-    logger.debug("Done loading concepts!");
   }
 
   /**
