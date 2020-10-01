af5b9f4d8a6b319b30f1e3322c7ca86c5c8ce78c
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2020-10-01 12:30:42 -0700

    adding start of directory load service

---
+++ src/main/java/gov/nih/nci/evs/api/service/DirectoryElasticLoadService.java
@@ -0,0 +1,35 @@
+package gov.nih.nci.evs.api.service;
+
+import java.io.IOException;
+
+import gov.nih.nci.evs.api.model.Terminology;
+import gov.nih.nci.evs.api.support.es.ElasticLoadConfig;
+import gov.nih.nci.evs.api.util.HierarchyUtils;
+
+/**
+ * The service to load concepts to Elasticsearch
+ * 
+ * Retrieves concepts from stardog and creates necessary index on Elasticsearch
+ * 
+ * @author Arun
+ *
+ */
+public interface DirectoryElasticLoadService {
+  /**
+   * Load cached objects to elasticsearch.
+   * 
+   * @param config the load config from command line input
+   * @param terminology the terminology
+   * @throws IOException the io exception
+   */
+  void loadObjects(ElasticLoadConfig config, Terminology terminology, HierarchyUtils hierarchy) throws IOException;
+  
+  /**
+   * Load concepts to elasticsearch.
+   * 
+   * @param config the load config from command line input
+   * @param terminology the terminology
+   * @throws IOException the io exception
+   */
+  void loadConcepts(ElasticLoadConfig config, Terminology terminology, HierarchyUtils hierarchy) throws IOException;
+}
---
+++ src/main/java/gov/nih/nci/evs/api/service/DirectoryElasticLoadServiceImpl.java
@@ -0,0 +1,103 @@
+
+package gov.nih.nci.evs.api.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import gov.nih.nci.evs.api.Application;
+import gov.nih.nci.evs.api.model.Concept;
+import gov.nih.nci.evs.api.model.ConceptMinimal;
+import gov.nih.nci.evs.api.model.IncludeParam;
+import gov.nih.nci.evs.api.model.Terminology;
+import gov.nih.nci.evs.api.support.es.ElasticLoadConfig;
+import gov.nih.nci.evs.api.support.es.ElasticObject;
+import gov.nih.nci.evs.api.support.es.IndexMetadata;
+import gov.nih.nci.evs.api.util.HierarchyUtils;
+import gov.nih.nci.evs.api.util.TerminologyUtils;
+
+/**
+ * The implementation for {@link DirectoryElasticLoadService}.
+ *
+ * @author Arun
+ */
+@Service
+public class DirectoryElasticLoadServiceImpl implements ElasticLoadService {
+
+  /** the logger *. */
+  private static final Logger logger = LoggerFactory.getLogger(DirectoryElasticLoadServiceImpl.class);
+
+  /** the concepts download location *. */
+  @Value("${nci.evs.bulkload.conceptsDir}")
+  private String CONCEPTS_OUT_DIR;
+
+  /** the lock file name *. */
+  @Value("${nci.evs.bulkload.lockFile}")
+  private String LOCK_FILE;
+
+  /** download batch size *. */
+  @Value("${nci.evs.bulkload.downloadBatchSize}")
+  private int DOWNLOAD_BATCH_SIZE;
+
+  /** index batch size *. */
+  @Value("${nci.evs.bulkload.indexBatchSize}")
+  private int INDEX_BATCH_SIZE;
+
+  /** the environment *. */
+  @Autowired
+  Environment env;
+
+  /** The Elasticsearch operations service instance *. */
+  @Autowired
+  ElasticOperationsService operationsService;
+
+  /** The sparql query manager service. */
+  @Autowired
+  private SparqlQueryManagerService sparqlQueryManagerService;
+
+  /** The elasticsearch query service *. */
+  @Autowired
+  private ElasticQueryService esQueryService;
+
+  /** The term utils. */
+  /* The terminology utils */
+  @Autowired
+  private TerminologyUtils termUtils;
+
+  /* see superclass */
+  @Override
+  public void loadConcepts(ElasticLoadConfig config, Terminology terminology,
+    HierarchyUtils hierarchy) throws IOException {
+
+  }
+ 
+
+  /* see superclass */
+  @Override
+  public void loadObjects(ElasticLoadConfig config, Terminology terminology,
+    HierarchyUtils hierarchy) throws IOException {
+    
+  }
+}
