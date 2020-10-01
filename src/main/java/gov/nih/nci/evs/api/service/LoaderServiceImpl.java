c3826b1abb8df6f92066328e554b68edead999f2
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2020-10-01 12:13:11 -0700

    creating LoaderService

---
+++ src/main/java/gov/nih/nci/evs/api/service/LoaderServiceImpl.java
@@ -0,0 +1,161 @@
+
+package gov.nih.nci.evs.api.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import gov.nih.nci.evs.api.Application;
+import gov.nih.nci.evs.api.model.Concept;
+import gov.nih.nci.evs.api.model.ConceptMinimal;
+import gov.nih.nci.evs.api.model.IncludeParam;
+import gov.nih.nci.evs.api.model.Terminology;
+import gov.nih.nci.evs.api.support.es.ElasticLoadConfig;
+import gov.nih.nci.evs.api.support.es.ElasticObject;
+import gov.nih.nci.evs.api.support.es.IndexMetadata;
+import gov.nih.nci.evs.api.util.HierarchyUtils;
+import gov.nih.nci.evs.api.util.TerminologyUtils;
+
+/**
+ * The implementation for {@link LoaderService}.
+ *
+ * @author Arun
+ */
+@Service
+public class LoaderServiceImpl implements LoaderService {
+
+  /** the logger *. */
+  private static final Logger logger = LoggerFactory.getLogger(LoaderServiceImpl.class);
+
+  /** the concepts download location *. */
+  @Value("${nci.evs.bulkload.conceptsDir}")
+  private String CONCEPTS_OUT_DIR;
+
+  /** the lock file name *. */
+  @Value("${nci.evs.bulkload.lockFile}")
+  private String LOCK_FILE;
+
+  /** download batch size *. */
+  @Value("${nci.evs.bulkload.downloadBatchSize}")
+  private int DOWNLOAD_BATCH_SIZE;
+
+  /** index batch size *. */
+  @Value("${nci.evs.bulkload.indexBatchSize}")
+  private int INDEX_BATCH_SIZE;
+
+  /** the environment *. */
+  @Autowired
+  Environment env;
+
+  /** The Elasticsearch operations service instance *. */
+  @Autowired
+  ElasticOperationsService operationsService;
+
+  /** The sparql query manager service. */
+  @Autowired
+  private SparqlQueryManagerService sparqlQueryManagerService;
+
+  /** The elasticsearch query service *. */
+  @Autowired
+  private ElasticQueryService esQueryService;
+
+  /** The term utils. */
+  /* The terminology utils */
+  @Autowired
+  private TerminologyUtils termUtils;
+  
+  /**
+   * prepare command line options available.
+   *
+   * @return the options
+   */
+  private static Options prepareOptions() {
+    Options options = new Options();
+
+    options.addOption("f", "forceDeleteIndex", false,
+        "Force delete index if index already exists.");
+    options.addOption("h", "help", false, "Show this help information and exit.");
+    options.addOption("r", "realTime", false, "Keep for backwards compabitlity. No Effect.");
+    options.addOption("t", "terminology", true, "The terminology (ex: ncit_20.02d) to load.");
+
+    return options;
+  }
+  
+  /**
+   * print command line help information.
+   *
+   * @param options the options available
+   */
+  private static void printHelp(Options options) {
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("java -jar $DIR/evsrestapi-*.jar", options);
+    return;
+  }
+  
+  /**
+   * build config object from command line options.
+   *
+   * @param cmd the command line object
+   * @param defaultLocation the default download location to use
+   * @return the config object
+   */
+  private static ElasticLoadConfig buildConfig(CommandLine cmd, String defaultLocation) {
+    ElasticLoadConfig config = new ElasticLoadConfig();
+
+    config.setTerminology(cmd.getOptionValue('t'));
+    config.setRealTime(cmd.hasOption('r'));
+    config.setForceDeleteIndex(cmd.hasOption('f'));
+    if (cmd.hasOption('l')) {
+      String location = cmd.getOptionValue('l');
+      if (StringUtils.isBlank(location)) {
+        logger.error("Location is empty!");
+
+      }
+      if (!location.endsWith("/")) {
+        location += "/";
+      }
+      logger.info("location - {}", location);
+      config.setLocation(location);
+    } else {
+      config.setLocation(defaultLocation);
+    }
+
+    return config;
+  }
+
+  /**
+   * the main method to trigger elasticsearch load via command line *.
+   *
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+  }
+
+  /**
+   * prepare command line options available.
+   *
+   * @return the options
+   */
+}
