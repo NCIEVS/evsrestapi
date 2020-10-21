1c1c845380c894fbae59754233f98beaf13750cf
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2020-10-21 23:13:41 +0100

    MErge with develop

--- src/main/java/gov/nih/nci/evs/api/model/Terminology.java
+++ src/main/java/gov/nih/nci/evs/api/model/Terminology.java
@@ -28,13 +28,13 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @JsonInclude(Include.NON_EMPTY)
 public class Terminology extends BaseModel {
 
-	/** The terminology. */
-	@Field(type = FieldType.Keyword)
-	private String terminology;
+  /** The terminology. */
+  @Field(type = FieldType.Keyword)
+  private String terminology;
 
-	/** The version. */
-	@Field(type = FieldType.Keyword)
-	private String version;
+  /** The version. */
+  @Field(type = FieldType.Keyword)
+  private String version;
 
   /** The date. */
   @Field(type = FieldType.Date)
@@ -44,13 +44,10 @@ public class Terminology extends BaseModel {
   @Field(type = FieldType.Keyword)
   private String name;
 
-	/** The name. */
-	@Field(type = FieldType.Keyword)
-	private String name;
+  /** The description. */
+  @Field(type = FieldType.Text)
+  private String description;
 
-	/** The description. */
-	@Field(type = FieldType.Text)
-	private String description;
   /** The graph. */
   @Field(type = FieldType.Keyword)
   private String graph;
@@ -59,25 +56,22 @@ public class Terminology extends BaseModel {
   @Field(type = FieldType.Keyword)
   private String source;
 
-	/** The graph. */
-	@Field(type = FieldType.Keyword)
-	private String graph;
+  /** The terminology version. */
+  @Field(type = FieldType.Keyword)
+  private String terminologyVersion;
 
-	/** The graph source. */
-	@Field(type = FieldType.Keyword)
-	private String source;
+  /** The latest. */
+  @Field(type = FieldType.Boolean)
+  private Boolean latest;
 
-	/** The terminology version. */
-	@Field(type = FieldType.Keyword)
-	private String terminologyVersion;
+  /** The tags. */
+  @Field(type = FieldType.Text)
+  private Map<String, String> tags;
 
-	/** The latest. */
-	@Field(type = FieldType.Boolean)
-	private Boolean latest;
+  /** The index name for concepts. */
+  @Field(type = FieldType.Keyword)
+  private String indexName;
 
-	/** The tags. */
-	@Field(type = FieldType.Text)
-	private Map<String, String> tags;
   /** The index name for generic objects. */
   @Field(type = FieldType.Keyword)
   private String objectIndexName;
@@ -93,13 +87,15 @@ public class Terminology extends BaseModel {
     this.terminology = "ncit";
   }
 
-	/** The index name for concepts. */
-	@Field(type = FieldType.Keyword)
-	private String indexName;
+  /**
+   * Instantiates a {@link Terminology} from the specified parameters.
+   *
+   * @param other the other
+   */
+  public Terminology(final Terminology other) {
+    populateFrom(other);
+  }
 
-	/** The index name for generic objects. */
-	@Field(type = FieldType.Keyword)
-	private String objectIndexName;
   /**
    * Populate from.
    *
@@ -130,60 +126,42 @@ public class Terminology extends BaseModel {
     return terminology;
   }
 
-	/**
-	 * Instantiates an empty {@link Terminology}.
-	 */
-	public Terminology() {
-		// n/a
-		this.terminology = "ncit";
-	}
+  /**
+   * Sets the terminology.
+   *
+   * @param terminology the terminology
+   */
+  public void setTerminology(final String terminology) {
+    this.terminology = terminology;
+  }
 
-	/**
-	 * Instantiates a {@link Terminology} from the specified parameters.
-	 *
-	 * @param other the other
-	 */
-	public Terminology(final Terminology other) {
-		populateFrom(other);
-	}
+  /**
+   * Returns the version.
+   *
+   * @return the version
+   */
+  public String getVersion() {
+    return version;
+  }
 
-	/**
-	 * Populate from.
-	 *
-	 * @param other the other
-	 */
-	public void populateFrom(final Terminology other) {
-		terminology = other.getTerminology();
-		version = other.getVersion();
-		date = other.getDate();
-		name = other.getName();
-		description = other.getDescription();
-		graph = other.getGraph();
-		source = other.getSource();
-		terminologyVersion = other.getTerminologyVersion();
-		latest = other.getLatest();
-		tags = new HashMap<>(other.getTags());
-		indexName = other.getIndexName();
-		objectIndexName = other.getObjectIndexName();
-	}
+  /**
+   * Sets the version.
+   *
+   * @param version the version
+   */
+  public void setVersion(final String version) {
+    this.version = version;
+  }
 
-	/**
-	 * Returns the terminology.
-	 *
-	 * @return the terminology
-	 */
-	public String getTerminology() {
-		return terminology;
-	}
+  /**
+   * Returns the date.
+   *
+   * @return the date
+   */
+  public String getDate() {
+    return date;
+  }
 
-	/**
-	 * Sets the terminology.
-	 *
-	 * @param terminology the terminology
-	 */
-	public void setTerminology(final String terminology) {
-		this.terminology = terminology;
-	}
   /**
    * Sets the date.
    *
@@ -202,68 +180,60 @@ public class Terminology extends BaseModel {
     return name;
   }
 
-	/**
-	 * Returns the version.
-	 *
-	 * @return the version
-	 */
-	public String getVersion() {
-		return version;
-	}
+  /**
+   * Sets the name.
+   *
+   * @param name the name
+   */
+  public void setName(final String name) {
+    this.name = name;
+  }
 
-	/**
-	 * Sets the version.
-	 *
-	 * @param version the version
-	 */
-	public void setVersion(final String version) {
-		this.version = version;
-	}
+  /**
+   * Returns the description.
+   *
+   * @return the description
+   */
+  public String getDescription() {
+    return description;
+  }
 
-	/**
-	 * Returns the date.
-	 *
-	 * @return the date
-	 */
-	public String getDate() {
-		return date;
-	}
+  /**
+   * Sets the description.
+   *
+   * @param description the description
+   */
+  public void setDescription(final String description) {
+    this.description = description;
+  }
 
-	/**
-	 * Sets the date.
-	 *
-	 * @param date the date
-	 */
-	public void setDate(String date) {
-		this.date = date;
-	}
+  /**
+   * Returns the graph.
+   *
+   * @return the graph
+   */
+  public String getGraph() {
+    return graph;
+  }
 
-	/**
-	 * Returns the name.
-	 *
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+  /**
+   * Sets the graph.
+   *
+   * @param graph the graph
+   */
+  public void setGraph(final String graph) {
+    this.graph = graph;
+  }
 
-	/**
-	 * Sets the name.
-	 *
-	 * @param name the name
-	 */
-	public void setName(final String name) {
-		this.name = name;
-	}
+  /**
+   * Returns the source.
+   *
+   * @return the source
+   */
+  public String getSource() {
+    return source;
+  }
 
-	/**
-	 * Returns the description.
-	 *
-	 * @return the description
-	 */
-	public String getDescription() {
-		return description;
-	}
   /**
    * Sets the source.
    *
@@ -285,107 +255,31 @@ public class Terminology extends BaseModel {
     return terminologyVersion;
   }
 
-	/**
-	 * Sets the description.
-	 *
-	 * @param description the description
-	 */
-	public void setDescription(final String description) {
-		this.description = description;
-	}
-
-	/**
-	 * Returns the graph.
-	 *
-	 * @return the graph
-	 */
-	public String getGraph() {
-		return graph;
-	}
-
-	/**
-	 * Sets the graph.
-	 *
-	 * @param graph the graph
-	 */
-	public void setGraph(final String graph) {
-		this.graph = graph;
-	}
-
-	/**
-	 * Returns the source.
-	 *
-	 * @return the source
-	 */
-	public String getSource() {
-		return source;
-	}
-
-	/**
-	 * Sets the source.
-	 *
-	 * @param source the source
-	 */
-	public void setSource(final String source) {
-		this.source = source;
-	}
-
-	/**
-	 * Returns the terminology version.
-	 *
-	 * @return the terminology version
-	 */
-	public String getTerminologyVersion() {
-		if (StringUtils.isEmpty(terminologyVersion)) {
-			terminologyVersion = terminology + "_" + version;
-		}
-		return terminologyVersion;
-	}
-
-	/**
-	 * Sets the terminology version.
-	 *
-	 * @param terminologyVersion the terminology version
-	 */
-	public void setTerminologyVersion(final String terminologyVersion) {
-		this.terminologyVersion = terminologyVersion;
-	}
-
-	/**
-	 * Returns the latest.
-	 *
-	 * @return the latest
-	 */
-	public Boolean getLatest() {
-		return latest;
-	}
   /**
-   * Returns the index name for objects.
-   * 
-   * @return the object index name
+   * Sets the terminology version.
+   *
+   * @param terminologyVersion the terminology version
    */
-  public String getObjectIndexName() {
-    if (StringUtils.isEmpty(objectIndexName)) {
-      objectIndexName = "evs_object_" + getTerminologyVersion().replaceAll("[^a-zA-Z0-9_]", "");
-    }
-    return objectIndexName;
+  public void setTerminologyVersion(final String terminologyVersion) {
+    this.terminologyVersion = terminologyVersion;
   }
 
-	/**
-	 * Sets the latest.
-	 *
-	 * @param latest the latest
-	 */
-	public void setLatest(final Boolean latest) {
-		this.latest = latest;
-	}
   /**
-   * Sets the index name for objects.
-   * 
-   * @param objectIndexName the object index name
+   * Returns the latest.
+   *
+   * @return the latest
    */
-  public void setObjectIndexName(String objectIndexName) {
-    this.objectIndexName = objectIndexName;
+  public Boolean getLatest() {
+    return latest;
+  }
+
+  /**
+   * Sets the latest.
+   *
+   * @param latest the latest
+   */
+  public void setLatest(final Boolean latest) {
+    this.latest = latest;
   }
 
   /**
@@ -406,6 +300,69 @@ public class Terminology extends BaseModel {
     this.metadata = metadata;
   }
 
+  /**
+   * Returns the tags.
+   *
+   * @return the tags
+   */
+  public Map<String, String> getTags() {
+    if (tags == null) {
+      tags = new HashMap<>();
+    }
+    return tags;
+  }
+
+  /**
+   * Sets the tags.
+   *
+   * @param tags the tags
+   */
+  public void setTags(final Map<String, String> tags) {
+    this.tags = tags;
+  }
+
+  /**
+   * Returns the index name for concepts belonging to this terminology.
+   * 
+   * @return the index name
+   */
+  public String getIndexName() {
+    if (StringUtils.isEmpty(indexName)) {
+      indexName = "concept_" + getTerminologyVersion().replaceAll("[^a-zA-Z0-9_]", "");
+    }
+    return indexName;
+  }
+
+  /**
+   * Sets the index name for concepts belonging to this terminology.
+   * 
+   * @param indexName the index name
+   */
+  public void setIndexName(String indexName) {
+    this.indexName = indexName;
+  }
+
+  /**
+   * Returns the index name for objects.
+   * 
+   * @return the object index name
+   */
+  public String getObjectIndexName() {
+    if (StringUtils.isEmpty(objectIndexName)) {
+      objectIndexName = "evs_object_" + getTerminologyVersion().replaceAll("[^a-zA-Z0-9_]", "");
+    }
+    return objectIndexName;
+  }
+
+  /**
+   * Sets the index name for objects.
+   * 
+   * @param objectIndexName the object index name
+   */
+  public void setObjectIndexName(String objectIndexName) {
+    this.objectIndexName = objectIndexName;
+  }
+
   /* see superclass */
   @Override
   public int hashCode() {
@@ -424,179 +381,6 @@ public class Terminology extends BaseModel {
     result = prime * result + ((objectIndexName == null) ? 0 : objectIndexName.hashCode());
     return result;
   }
-
-	/**
-	 * Returns the tags.
-	 *
-	 * @return the tags
-	 */
-	public Map<String, String> getTags() {
-		if (tags == null) {
-			tags = new HashMap<>();
-		}
-		return tags;
-	}
-
-	/**
-	 * Sets the tags.
-	 *
-	 * @param tags the tags
-	 */
-	public void setTags(final Map<String, String> tags) {
-		this.tags = tags;
-	}
-
-	/**
-	 * Returns the index name for concepts belonging to this terminology.
-	 * 
-	 * @return the index name
-	 */
-	public String getIndexName() {
-		if (StringUtils.isEmpty(indexName)) {
-			indexName = "concept_" + getTerminologyVersion().replaceAll("[^a-zA-Z0-9_]", "");
-		}
-		return indexName;
-	}
-
-	/**
-	 * Sets the index name for concepts belonging to this terminology.
-	 * 
-	 * @param indexName the index name
-	 */
-	public void setIndexName(String indexName) {
-		this.indexName = indexName;
-	}
-
-	/**
-	 * Returns the index name for objects.
-	 * 
-	 * @return the object index name
-	 */
-	public String getObjectIndexName() {
-		if (StringUtils.isEmpty(objectIndexName)) {
-			objectIndexName = "evs_object_" + getTerminologyVersion().replaceAll("[^a-zA-Z0-9_]", "");
-		}
-		return objectIndexName;
-	}
-
-	/**
-	 * Sets the index name for objects.
-	 * 
-	 * @param objectIndexName the object index name
-	 */
-	public void setObjectIndexName(String objectIndexName) {
-		this.objectIndexName = objectIndexName;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((graph == null) ? 0 : graph.hashCode());
-		result = prime * result + ((source == null) ? 0 : source.hashCode());
-		result = prime * result + ((latest == null) ? 0 : latest.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((terminology == null) ? 0 : terminology.hashCode());
-		result = prime * result + ((terminologyVersion == null) ? 0 : terminologyVersion.hashCode());
-		result = prime * result + ((version == null) ? 0 : version.hashCode());
-		result = prime * result + ((date == null) ? 0 : date.hashCode());
-		result = prime * result + ((indexName == null) ? 0 : indexName.hashCode());
-		result = prime * result + ((objectIndexName == null) ? 0 : objectIndexName.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final Terminology other = (Terminology) obj;
-		if (description == null) {
-			if (other.description != null) {
-				return false;
-			}
-		} else if (!description.equals(other.description)) {
-			return false;
-		}
-		if (graph == null) {
-			if (other.graph != null) {
-				return false;
-			}
-		} else if (!graph.equals(other.graph)) {
-			return false;
-		}
-		if (source == null) {
-			if (other.source != null) {
-				return false;
-			}
-		} else if (!source.equals(other.source)) {
-			return false;
-		}
-		if (latest == null) {
-			if (other.latest != null) {
-				return false;
-			}
-		} else if (!latest.equals(other.latest)) {
-			return false;
-		}
-		if (name == null) {
-			if (other.name != null) {
-				return false;
-			}
-		} else if (!name.equals(other.name)) {
-			return false;
-		}
-		if (terminology == null) {
-			if (other.terminology != null) {
-				return false;
-			}
-		} else if (!terminology.equals(other.terminology)) {
-			return false;
-		}
-		if (terminologyVersion == null) {
-			if (other.terminologyVersion != null) {
-				return false;
-			}
-		} else if (!terminologyVersion.equals(other.terminologyVersion)) {
-			return false;
-		}
-		if (version == null) {
-			if (other.version != null) {
-				return false;
-			}
-		} else if (!version.equals(other.version)) {
-			return false;
-		}
-		if (date == null) {
-			if (other.date != null) {
-				return false;
-			}
-		} else if (!date.equals(other.date)) {
-			return false;
-		}
-		if (indexName == null) {
-			if (other.indexName != null) {
-				return false;
-			}
-		} else if (!indexName.equals(other.indexName)) {
-			return false;
-		}
-		if (objectIndexName == null) {
-			if (other.objectIndexName != null) {
-				return false;
-			}
-		} else if (!objectIndexName.equals(other.objectIndexName)) {
-			return false;
-		}
-		return true;
-	}
 
   /* see superclass */
   @Override
