83e53de94dfedf4b758551504b9e02939961f8b6
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2020-12-08 23:26:20 +0000

    Change position of  in stardog graph

--- src/main/bin/Monthly_Load.sh
+++ src/main/bin/Monthly_Load.sh
@@ -9,8 +9,9 @@ version=`unzip -p /local/content/downloads/ThesaurusInferred_forTS.zip "*Thesaur
 # Use something like this to find versions to remove - and remove by graph instead of --all
 # ./stardog query execute NCIT2 "select distinct ?graphName where { graph ?graphName {?s ?p ?o} }" | grep http | grep -v $version
 
+graph=http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus$version.owl
 ./stardog data remove --all CTRP -u $USER -p $PASSWORD
-./stardog data add CTRP -g http://ncicb.nci.nih.gov/xml/owl/EVS/$version/Thesaurus.owl /local/content/downloads/ThesaurusInferred_forTS.zip -u $USER -p $PASSWORD
+./stardog data add CTRP -g $graph /local/content/downloads/ThesaurusInferred_forTS.zip -u $USER -p $PASSWORD
 
 ./stardog data remove --all NCIT2 -u $USER -p $PASSWORD
-./stardog data add NCIT2 -g http://ncicb.nci.nih.gov/xml/owl/EVS/$version/Thesaurus.owl /local/content/downloads/ThesaurusInferred_forTS.zip -u $USER -p $PASSWORD
+./stardog data add NCIT2 -g $graph /local/content/downloads/ThesaurusInferred_forTS.zip -u $USER -p $PASSWORD
