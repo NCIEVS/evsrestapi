94d1cae500d9d05fbaac72c2425a52aa3396a600
kimongl <kim.ong@nih.gov>
2020-12-31 13:59:29 -0500

    Updated OWLSPARQLUtils.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/OWLSPARQLUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/OWLSPARQLUtils.java
@@ -5582,6 +5582,35 @@ Term Type
 		return new SortUtils().quickSort(v);
 	}
 
+	public String construct_get_property_values(String named_graph, String propertyName) {
+			String prefixes = getPrefixes();
+			StringBuffer buf = new StringBuffer();
+			buf.append(prefixes);
+			buf.append("").append("\n");
+			buf.append("SELECT distinct ?p_label ?p_value").append("\n");
+			buf.append("{").append("\n");
+			buf.append("    graph <" + named_graph + "> {").append("\n");
+			buf.append("            ?x a owl:Class .").append("\n");
+			buf.append("            ?x :NHC0 ?x_code .").append("\n");
+			buf.append("            ?x rdfs:label ?x_label .").append("\n");
+			buf.append("").append("\n");
+			buf.append("            ?p a owl:AnnotationProperty .").append("\n");
+			buf.append("            ?x ?p ?p_value .").append("\n");
+			buf.append("            ?p rdfs:label ?p_label .").append("\n");
+			buf.append("            ?p rdfs:label \"" + propertyName + "\"^^xsd:string .").append("\n");
+			buf.append("     }").append("\n");
+			buf.append("}").append("\n");
+			return buf.toString();
+	}
 
+
+	public Vector getAvailablePropertyValues(String named_graph, String propertyName) {
+			String query = construct_get_property_values(named_graph, propertyName);
+			Vector v = executeQuery(query);
+			if (v == null) return null;
+			if (v.size() == 0) return v;
+			v = new ParserUtils().getResponseValues(v);
+			return new SortUtils().quickSort(v);
+	}
 }
 
