03559553a3fdb355518b09e7e25a2a1483425ae9
kimongl <kim.ong@nih.gov>
2020-12-31 05:35:04 -0500

    Added HyperlinkHelper.java.

---
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/HyperlinkHelper.java
@@ -0,0 +1,30 @@
+package gov.nih.nci.evs.restapi.util;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+public class HyperlinkHelper {
+	private static String HYPER_LINK = "https://nciterms65.nci.nih.gov/ncitbrowser/pages/concept_details.jsf?dictionary=NCI_Thesaurus&version=20.11e&ns=ncit&code=";
+
+	public static String toHyperlink(String code) {
+		return toHyperlink(HYPER_LINK, code);
+	}
+
+	public static String toHyperlink(String hyperlink, String code) {
+		StringBuffer buf = new StringBuffer();
+		buf.append("<a ");
+		buf.append("href=");
+		buf.append("\"");
+		buf.append(hyperlink + code);
+		buf.append("\">");
+		buf.append(code);
+		buf.append("</a>");
+		return buf.toString();
+	}
+
+	public static void main(String[] args) {
+		String code = "C12345";
+		System.out.println(toHyperlink(code));
+	}
+}
