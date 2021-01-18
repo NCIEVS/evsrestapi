1eaa4f2504a0aadfac6a5bb8d06de79742a7eb82
kimongl <kim.ong@nih.gov>
2021-01-18 11:29:15 -0500

    Updated ExcelUtils.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ExcelUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ExcelUtils.java
@@ -355,6 +355,69 @@ public class ExcelUtils {
 		return workbook;
 	}
 
+	public static Workbook deleteColumn(Workbook workbook, int sheetIndex, int columnToDelete ){
+		Sheet sheet = workbook.getSheetAt(sheetIndex);
+        int maxColumn = 0;
+        for (int r=0; r < sheet.getLastRowNum()+1; r++){
+            Row row = sheet.getRow(r);
+            // if no row exists here; then nothing to do; next!
+            if ( row == null )
+                continue;
+
+            // if the row doesn't have this many columns then we are good; next!
+            int lastColumn = row.getLastCellNum();
+            if (lastColumn > maxColumn )
+                maxColumn = lastColumn;
+
+            if (lastColumn < columnToDelete )
+                continue;
+
+            for (int x=columnToDelete+1; x < lastColumn + 1; x++){
+                Cell oldCell    = row.getCell(x-1);
+                if ( oldCell != null ) {
+                    row.removeCell( oldCell );
+				}
+                Cell nextCell = row.getCell(x);
+                if ( nextCell != null ){
+                    Cell newCell    = row.createCell( x-1, nextCell.getCellType());
+                    cloneCell(newCell, nextCell);
+                }
+            }
+        }
+        // Adjust the column widths
+        for ( int c=0; c < maxColumn; c++ ){
+            sheet.setColumnWidth( c, sheet.getColumnWidth(c+1) );
+        }
+        return workbook;
+    }
+
+	private static void cloneCell( Cell cNew, Cell cOld ){
+		cNew.setCellComment( cOld.getCellComment() );
+		cNew.setCellStyle( cOld.getCellStyle() );
+		switch ( cNew.getCellType() ){
+			case Cell.CELL_TYPE_BOOLEAN:{
+				cNew.setCellValue( cOld.getBooleanCellValue() );
+				break;
+			}
+			case Cell.CELL_TYPE_NUMERIC:{
+				cNew.setCellValue( cOld.getNumericCellValue() );
+				break;
+			}
+			case Cell.CELL_TYPE_STRING:{
+				cNew.setCellValue( cOld.getStringCellValue() );
+				break;
+			}
+			case Cell.CELL_TYPE_ERROR:{
+				cNew.setCellValue( cOld.getErrorCellValue() );
+				break;
+			}
+			case Cell.CELL_TYPE_FORMULA:{
+				cNew.setCellFormula( cOld.getCellFormula() );
+			   break;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		String excelfile = args[0];
 		System.out.println("excelfile: " + excelfile);
