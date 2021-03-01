f3fd2910ddc307dd7d1fbc88494fac7c1fde3fd6
kimongl <kim.ong@nih.gov>
2021-03-01 07:19:24 -0500

    Added HTTPGetRequest.java.

---
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/HTTPGetRequest.java
@@ -0,0 +1,166 @@
+package gov.nih.nci.evs.restapi.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+public class HTTPGetRequest {
+
+	private static final String USER_AGENT = "Mozilla/5.0";
+	private static String ALPHABETIC = "abcdefghijklmnopqrstuvwxyz";
+
+    private String GET_URL = "https://druginfo.nlm.nih.gov/drugportal/drug/names/";
+
+    private int number_of_columns = 0;
+    private Vector data_vec = new Vector();
+
+    public HTTPGetRequest() {
+
+	}
+
+	public void setGET_URL(String GET_URL) {
+		this.GET_URL = GET_URL;
+	}
+
+/*
+    private String extractTable(String line) {
+		int n = line.indexOf("<table");
+		line = line.substring(n, line.length());
+		n = line.lastIndexOf("</table>");
+		line = line.substring(0, n + "</table>".length());
+		return line;
+	}
+
+    private Vector extractTableHeadings(String line) {
+		Vector th_vec = new Vector();
+		int n = line.indexOf("</th>");
+		while (n != -1) {
+			String t = line.substring(0, n);
+			int m = t.lastIndexOf(">");
+			String s = t.substring(m+1, n);
+			if (s.length() > 0) {
+				if (!th_vec.contains(s)) {
+					th_vec.add(s);
+				} else {
+					break;
+				}
+			} else {
+				th_vec.add(s);
+			}
+
+			line = line.substring(n+1, line.length());
+			n = line.indexOf("</th>");
+		}
+		return th_vec;
+	}
+
+    private Vector extractTableData(String line) {
+		Vector td_vec = new Vector();
+		int n = line.indexOf("</td>");
+		while (n != -1) {
+			String t = line.substring(0, n);
+			int m = t.lastIndexOf(">");
+			String s = t.substring(m+1, n);
+			td_vec.add(s);
+			line = line.substring(n+1, line.length());
+			n = line.indexOf("</td>");
+		}
+		return td_vec;
+	}
+*/
+
+	public Vector extractRowData(Vector w) {
+		Vector v = new Vector();
+		for (int i=0; i<w.size(); i++) {
+			String line = (String) w.elementAt(i);
+			line = line.trim();
+			if (line.startsWith("<tr><td align=\"left\"><a href=")) {
+				line = line.replace("/drugportal", "https://druginfo.nlm.nih.gov/drugportal");
+				v.add(line);
+			}
+		}
+		return v;
+	}
+
+
+	public Vector sendGET(char c) throws IOException {
+		Vector w = new Vector();
+		URL obj = new URL(GET_URL + c);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				w.add(inputLine);
+			}
+			in.close();
+		} else {
+			System.out.println("GET request not worked");
+		}
+		return w;
+	}
+
+	public String getAlphabeticListing() {
+		StringBuffer buf = new StringBuffer();
+	    for (int i=0; i<ALPHABETIC.length(); i++) {
+			char ch = ALPHABETIC.charAt(i);
+			String t = "" + ch;
+			String t_upper = t.toUpperCase();
+			//&nbsp;
+			//https://druginfo.nlm.nih.gov/drugportal/drug/names
+			buf.append("<a href=\"https://druginfo.nlm.nih.gov/drugportal/drug/names/" + t + "\">" + t_upper + "</a> &nbsp;&nbsp;");
+		}
+		return buf.toString();
+	}
+
+    public void generate() {
+        Vector w = new Vector();
+        w.add("<html>");
+        w.add("<head>");
+        w.add("</head>");
+        w.add("<body style=\"background-color:LightGray;\">");
+        w.add("<h2>");
+        w.add("<center>FDA Approved Drugs</center>");
+        w.add("<h4>");
+        w.add("<center>(Last Updated on " + StringUtils.getToday() + ")</center>");
+        w.add("<center>");
+        w.add("<p></p>");
+        w.add(getAlphabeticListing());
+        w.add("<p></p>");
+        w.add("<table border=\"0\">");
+ 	    w.add("<tr><th scope=\"col\">Drug Names</th></tr>");
+	    for (int i=0; i<ALPHABETIC.length(); i++) {
+			char ch = ALPHABETIC.charAt(i);
+			try {
+				Vector v = sendGET(ch);
+				v = extractRowData(v);
+				w.addAll(v);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		w.add("</table>");
+		w.add("</center>");
+		w.add("</body>");
+        w.add("</html>");
+
+        Utils.saveToFile("FDAApprovedDrugs.html", w);
+	}
+
+	public static void main(String[] args) throws IOException {
+		long ms = System.currentTimeMillis();
+		HTTPGetRequest httpGetRequest = new HTTPGetRequest();
+        httpGetRequest.generate();
+		System.out.println("Total run time (ms): " + (System.currentTimeMillis() - ms));
+	}
+}
