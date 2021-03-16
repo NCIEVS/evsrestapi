0e491c1fb30144247bccfc936f95a80c7d96418f
kimongl <kim.ong@nih.gov>
2021-03-16 14:04:57 -0400

    Updated HierarchyHelper.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/HierarchyHelper.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/HierarchyHelper.java
@@ -753,6 +753,35 @@ public class HierarchyHelper implements Serializable {
 		return count;
 	}
 
+    public Vector get_transitive_closure_v3(String code) {
+		Vector w = new Vector();
+		int count = 0;
+		Vector v = null;
+		Stack stack = new Stack();
+		stack.push(code);
+		w.add(code);
+		HashSet hset = new HashSet();
+
+		while (!stack.isEmpty()) {
+			String next_code = (String) stack.pop();
+			if (!hset.contains(next_code)) {
+				hset.add(next_code);
+				count++;
+			}
+			v = getSubclassCodes(next_code);
+			if (v != null) {
+				w.addAll(v);
+				for (int i=0; i<v.size(); i++) {
+					String child_code = (String) v.elementAt(i);
+					stack.push(child_code);
+				}
+			}
+		}
+		hset.clear();
+		return w;
+	}
+
+
     public static void main(String[] args) {
 		Vector v = Utils.readFile("tvs_rel.txt");
 		HierarchyHelper test = new HierarchyHelper(v, 1);
