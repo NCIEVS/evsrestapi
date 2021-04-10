eeefee29d38aaa3ee1c2ed5fe78013549cace154
KIm Ong <kim.ong@nih.gov>
2021-04-10 07:17:52 -0400

    Added ExternalLinkPageGenerator.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/HyperlinkHelper.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/HyperlinkHelper.java
@@ -12,6 +12,7 @@ public class HyperlinkHelper {
 	}
 
 	public static String toHyperlink(String hyperlink, String code) {
+		if (hyperlink == null) return code;
 		StringBuffer buf = new StringBuffer();
 		buf.append("<a ");
 		buf.append("href=");
@@ -26,6 +27,11 @@ public class HyperlinkHelper {
 	public static void addHyperlinks(String filename, String hyperlinkfile) {
 	    Vector v = Utils.readFile(filename);
 	    Vector v2 = Utils.readFile(hyperlinkfile);
+	    addHyperlinks(filename, v2);
+	}
+
+	public static void addHyperlinks(String filename, Vector v2) {
+	    Vector v = Utils.readFile(filename);
 	    String t = (String) v2.elementAt(0);
 	    Vector hyperlinks = StringUtils.parseData(t, '|');
 	    Vector w0 = new Vector();
@@ -38,7 +44,6 @@ public class HyperlinkHelper {
 			for (int j=0; j<u.size(); j++) {
 				buf.append(toHyperlink((String) hyperlinks.elementAt(j),
 				                       (String) u.elementAt(j)));
-
 				if (j < u.size()-1) {
 				    buf.append("|");
 				}
@@ -49,7 +54,6 @@ public class HyperlinkHelper {
 		w0.addAll(w);
 		Utils.saveToFile(filename, w0);
 	}
-
 
 	public static void main(String[] args) {
 		String code = "C12345";
