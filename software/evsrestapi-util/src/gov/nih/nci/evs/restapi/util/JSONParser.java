2c4dd04e8e53d499a8cd14c249f52b46e1e5c578
kimongl <kim.ong@nih.gov>
2021-05-10 23:46:09 -0400

    Updated JSONParser.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/JSONParser.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/JSONParser.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 public class JSONParser {
 
@@ -137,4 +138,62 @@ public class JSONParser {
 		}
 	}
 
+	public static Vector run(String json) {
+		Vector v = new Vector();
+		try {
+			Map map = parse(json);
+			Iterator it = map.keySet().iterator();
+			while (it.hasNext()) {
+				String key = (String) it.next();
+				Object obj = map.get(key);
+				if (obj instanceof List) {
+					List list = (List) map.get(key);
+					if (list != null && list.size() > 0) {
+						Object obj1 = list.get(0);
+						if (obj1 instanceof String) {
+							for (int i=0; i<list.size(); i++) {
+								String t = (String) list.get(i);
+								v.add(key + ": " +  t);
+							}
+						} else {
+							for (int i=0; i<list.size(); i++) {
+								Object t = list.get(i);
+								v.add(key + ": " +  t.toString());
+							}
+						}
+					}
+
+				} else if (obj instanceof Map) {
+					Map map_obj = (Map) map.get(key);
+					if (map_obj != null && map_obj.keySet().size() > 0) {
+						Iterator it2 = map_obj.keySet().iterator();
+						Object map_obj_key1 = it2.next();
+						Object map_obj_value1 = map_obj.get(map_obj_key1);
+						if (map_obj_value1 instanceof String) {
+							Iterator it3 = map_obj.keySet().iterator();
+							while (it3.hasNext()) {
+								String key3 = (String) it3.next();
+								String value = (String) map_obj.get(key3);
+								//v.add(key3 + " --> " + value);
+								v.add(key3 + ": " + value);
+							}
+						} else {
+							Iterator it3 = map_obj.keySet().iterator();
+							while (it3.hasNext()) {
+								String key3 = (String) it3.next();
+								Object value = map_obj.get(key3);
+								//v.add(key3 + " --> " + value);
+								v.add(key3 + ": " + value.toString());
+							}
+						}
+					}
+				} else {
+					v.add(key + ": " + obj.toString());
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return new SortUtils().quickSort(v);
+	}
 }
