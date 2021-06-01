85cb9458110fd509c9dd3d88d56a2b479f2e2a02
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2021-06-02 00:34:07 +0100

    Revert unindexing of qualifiers

--- src/main/java/gov/nih/nci/evs/api/model/Definition.java
+++ src/main/java/gov/nih/nci/evs/api/model/Definition.java
@@ -38,7 +38,6 @@ public class Definition extends BaseModel implements Comparable<Definition> {
   private String source;
 
   /** The qualifiers. */
-  @Field(index = false)
   private List<Qualifier> qualifiers;
 
   /**
