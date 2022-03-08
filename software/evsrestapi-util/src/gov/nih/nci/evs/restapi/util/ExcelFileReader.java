a2c343ecfe082b92f82a637432a86d952def60b7
kimongl <kim.ong@nih.gov>
2022-03-08 10:03:02 -0500

    Updated ExcelFileReader.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ExcelFileReader.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ExcelFileReader.java
@@ -15,8 +15,61 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import java.util.*;
+import java.io.*;
 
 public class ExcelFileReader {
+
+	 public static void saveToFile(String outputfile, String t) {
+		 Vector v = new Vector();
+		 v.add(t);
+		 saveToFile(outputfile, v);
+	 }
+
+	 public static void saveToFile(String outputfile, Vector v) {
+
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(outputfile, "UTF-8");
+			if (v != null && v.size() > 0) {
+				for (int i=0; i<v.size(); i++) {
+					String t = (String) v.elementAt(i);
+					pw.println(t);
+				}
+		    }
+		} catch (Exception ex) {
+
+		} finally {
+			try {
+				pw.close();
+				System.out.println("Output file " + outputfile + " generated.");
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	 }
+
+	public static void saveToFile(PrintWriter pw, Vector v) {
+		if (v != null && v.size() > 0) {
+			for (int i=0; i<v.size(); i++) {
+				String t = (String) v.elementAt(i);
+				pw.println(t);
+			}
+		}
+	}
+
+	public String getSheetName(String fileName, int sheet_number) throws IOException {
+		File file = new File(fileName);
+		FileInputStream inputStream = new FileInputStream(file);
+		Workbook workbook = null;
+		String fileExtensionName = fileName.substring(fileName.indexOf("."));
+		if(fileExtensionName.equals(".xlsx")){
+			workbook = new XSSFWorkbook(inputStream);
+		} else if(fileExtensionName.equals(".xls")){
+			workbook = new HSSFWorkbook(inputStream);
+		}
+		Sheet sheet = workbook.getSheetAt(sheet_number);
+		return sheet.getSheetName();
+	}
 
 	public String getSheetName(String filePath, String fileName, int sheet_number) throws IOException {
 		File file = new File(filePath+"\\"+fileName);
@@ -32,13 +85,18 @@ public class ExcelFileReader {
 		return sheet.getSheetName();
 	}
 
-
 	public Vector readExcel(String filePath, String fileName, int sheet_number) throws IOException {
+		String absolutePath = filePath +"\\" + fileName;
+		return readExcel(absolutePath, sheet_number);
+	}
+
+	public Vector readExcel(String absolutePath, int sheet_number) throws IOException {
 		Vector w = new Vector();
-		File file = new File(filePath+"\\"+fileName);
+		File file = new File(absolutePath);
+
 		FileInputStream inputStream = new FileInputStream(file);
 		Workbook workbook = null;
-		String fileExtensionName = fileName.substring(fileName.indexOf("."));
+		String fileExtensionName = absolutePath.substring(absolutePath.indexOf("."));
 		if(fileExtensionName.equals(".xlsx")){
 			workbook = new XSSFWorkbook(inputStream);
 		} else if(fileExtensionName.equals(".xls")){
@@ -49,18 +107,22 @@ public class ExcelFileReader {
 		for (int i = 0; i < rowCount+1; i++) {
 			StringBuffer buf = new StringBuffer();
 			Row row = sheet.getRow(i);
-			for (int j = 0; j < row.getLastCellNum(); j++) {
-				try {
-					String retstr = getCellValue(row.getCell(j));
-					buf.append(retstr);
-					if (j < row.getLastCellNum()-1) {
+
+			if (row != null) {
+
+				for (int j = 0; j < row.getLastCellNum(); j++) {
+					try {
+						String retstr = getCellValue(row.getCell(j));
+						buf.append(retstr);
+						if (j < row.getLastCellNum()-1) {
+							buf.append("\t");
+						}
+					} catch (Exception ex) {
 						buf.append("\t");
 					}
-				} catch (Exception ex) {
-					buf.append("\t");
 				}
+				w.add(buf.toString());
 			}
-			w.add(buf.toString());
 		}
 		return w;
     }
@@ -95,11 +157,12 @@ public class ExcelFileReader {
 
     public static void main(String[] args) throws IOException{
 		ExcelFileReader test = new ExcelFileReader();
-		String excelfile = "PCDC_Terminology_2021-03-04.xls";
+		String excelfile = args[0];
+		String sheetIndex_str = args[1];
+		int sheet_number = Integer.parseInt(sheetIndex_str);
 		String filePath = System.getProperty("user.dir");
-		int sheet_number = 1;
 		Vector w = test.readExcel(filePath, excelfile, sheet_number);
 		String sheetName = test.getSheetName(filePath, excelfile, sheet_number);
-		Utils.saveToFile(sheetName + ".txt", w);
+		saveToFile(sheetName + ".txt", w);
     }
 }
