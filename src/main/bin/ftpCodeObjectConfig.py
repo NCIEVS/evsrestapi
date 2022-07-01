701bde03513776aebbd1d62024b847d92208fa65
peter-va <pvanaus@ucdavis.edu>
2022-07-01 14:50:15 -0700

    changes

--- src/main/bin/ftpCodeObjectConfig.py
+++ src/main/bin/ftpCodeObjectConfig.py
@@ -1,15 +1,25 @@
+from collections import OrderedDict
 import json
+import sys
 
 def ftpCodes() :
     ftpCodeObj = {}
-    with open("value_set_report_config.txt", encoding = 'utf-8') as f:
+    with open(sys.argv[1], encoding = 'utf-8') as f:
         lines = f.readlines()
         for line in lines:
-            code = line.split("|")[1].split("/")[-1]
+            code = line.split("|")[1].split("/")[-1];
             ftpCode = "/".join(line.split("|")[-2].split("/")[4:-1]) + "/";
             ftpCodeObj[code] = ftpCode;
-    print(json.dumps(ftpCodeObj, indent=4, sort_keys=True))
-
+            
+    with open("metadata/ncit.json") as ncit:
+        ncitData = json.load(ncit);
+    
+    ncitData["subsetLinks"] = ftpCodeObj;
+        
+    with open("metadata/ncit.json", "w+") as ncit:
+        ncit.write(json.dumps(OrderedDict(ncitData), indent=4, sort_keys=True))
 
 if __name__ == '__main__':
+    if(len(sys.argv) < 2):
+        sys.exit("Needs a file path argument: `python ftpCodeObjectConfig.py <file path>");
     ftpCodes()
