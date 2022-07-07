e683546f3221850fd1eb5cbe9f3e6ec4dda76141
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2022-07-07 01:02:47 +0100

    Start preparing scripts to work with GO

--- src/main/bin/Monthly_Load.sh
+++ src/main/bin/Monthly_Load.sh
@@ -2,6 +2,11 @@
 
 USER=xxxxroot
 PASSWORD=xxxxx
+dir=../data/UnitTestData
+
+#--------
+# Load NCIt - NCI Thesaurus
+#
 
 version=`unzip -p /local/content/downloads/ThesaurusInferred_forTS.zip "*ThesaurusInferred_forTS.owl" |\
    grep '<owl:versionInfo>' | perl -pe 's/.*<owl:versionInfo>//; s/<\/owl:versionInfo>//'`
@@ -15,3 +20,35 @@ graph=http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus$version.owl
 
 ./stardog data remove --all NCIT2 -u $USER -p $PASSWORD
 ./stardog data add NCIT2 -g $graph /local/content/downloads/ThesaurusInferred_forTS.zip -u $USER -p $PASSWORD
+
+#--------
+# Load GO - Gene Ontology
+#
+
+echo "  Set GO version and graph"
+version=`grep 'data-version' $dir/GO/go.obo | perl -pe 's/.*\: //; s/releases\///;'`
+graph=http://purl.obolibrary.org/obo/go${version}.owl
+echo"    version = $version"
+echo"    graph = $graph"
+
+# Clean and load stardog
+echo "  Remove stardog databases and load monthly/weekly"
+cat > $dir/x.sh << EOF
+#!/bin/bash
+echo "    load data"
+/opt/stardog/bin/stardog data add --named-graph http://GO_monthly NCIT2 /data/UnitTestData/GO/go.owl | sed 's/^/     /'
+echo "    optimize databases"
+/opt/stardog/bin/stardog-admin db optimize -n NCIT2 | sed 's/^/      /'
+EOF
+chmod 755 $dir/x.sh
+chmod ag+rwx $dir
+pid=`docker ps | grep stardog/stardog | cut -f 1 -d\  `
+# note: //data is required for gitbash
+docker exec -it $pid //data/UnitTestData/x.sh
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: problem loading stardog"
+    exit 1
+fi
+/bin/rm -f $dir/x.sh
+
+
