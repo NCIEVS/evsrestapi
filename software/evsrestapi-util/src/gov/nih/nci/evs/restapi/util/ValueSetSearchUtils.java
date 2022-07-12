19fbb76a182aadab407c9eba57f5958b39fc5ba7
kimongl <kim.ong@nih.gov>
2022-07-12 06:20:55 -0400

    Updated ValueSetSearchUtils.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ValueSetSearchUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ValueSetSearchUtils.java
@@ -27,6 +27,9 @@ public class ValueSetSearchUtils {
 	public static String STARTSWITH = "startsWith";
 	public static String CONTAINS = "contains";
 
+	public static String[] NAMES = new String[] {"label", "Display_Name", "Preferred_Name",
+	    "FULL_SYN", "Legacy Concept Name"};
+
 	public static String[] ALGORITHMS = new String[] {EXACT_MATCH, STARTSWITH, CONTAINS};
 
 	public ValueSetSearchUtils(String serviceUrl, String named_graph, String username, String password) {
@@ -186,7 +189,35 @@ public class ValueSetSearchUtils {
 	    return new ParserUtils().getResponseValues(v);
 	}
 
+	public Vector parseVSData(Vector v, boolean byName) {
+		//(1) Blue|C48333|Concept_In_Subset|CPTAC Baseline Medical Forms Terminology|C156953|FULL_SYN|Blue
+		List<String> nameList = new ArrayList<>(Arrays.asList(NAMES));
+		Vector w = new Vector();
+		for (int i=0; i<v.size(); i++) {
+			String line = (String) v.elementAt(i);
+			Vector u = StringUtils.parseData(line, '|');
+			String propertyName = (String) u.elementAt(5);
+			if (byName) {
+				if (nameList.contains(propertyName)) {
+					String t = (String) u.elementAt(3) + "|" + (String) u.elementAt(4);
+					if (!w.contains(t)) {
+						w.add(t);
+					}
+				}
+			} else {
+				if (!nameList.contains(propertyName)) {
+					String t = (String) u.elementAt(3) + "|" + (String) u.elementAt(4);
+					if (!w.contains(t)) {
+						w.add(t);
+					}
+				}
+			}
+		}
+		return new SortUtils().quickSort(w);
+	}
+
 	public static void main(String[] args) {
+		long ms = System.currentTimeMillis();
 		String serviceUrl = args[0];
 		String namedGraph = args[1];
 		String username = args[2];
@@ -199,11 +230,17 @@ public class ValueSetSearchUtils {
 		for (int i=0; i<ALGORITHMS.length; i++) {
 			String algorithm = ALGORITHMS[i];
 			Vector w = utils.search(namedGraph, term, algorithm);
-			Utils.dumpVector(algorithm + " name " + term, w);
+			Utils.dumpVector(algorithm + ", name - " + term, w);
+			boolean byName = true;
+			Vector w1 = utils.parseVSData(w, true);
+			Utils.dumpVector("value set, " + algorithm + ", name - " + term, w1);
+			Vector w2 = utils.parseVSData(w, false);
+			Utils.dumpVector("value set, " + algorithm + ", property - " + term, w2);
 		}
 
 		Vector w = utils.searchByCode(namedGraph, code);
-        Utils.dumpVector("EXACT_MATCH code " + code, w);
+        Utils.dumpVector("exactMatch code - " + code, w);
+        System.out.println("Total run time (ms): " + (System.currentTimeMillis() - ms));
 	}
 
 }
