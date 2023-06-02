7f302d63957d58e16c10c9547845558f34685d1d
peter-va <pvanaus@ucdavis.edu>
2023-06-01 17:15:10 -0700

    fix text issue in test

--- src/test/java/gov/nih/nci/evs/api/controller/HgncSampleTest.java
+++ src/test/java/gov/nih/nci/evs/api/controller/HgncSampleTest.java
@@ -74,7 +74,7 @@ public class HgncSampleTest extends SampleTest {
         terminologies.stream().filter(t -> t.getTerminology().equals("hgnc")).findFirst().get();
     assertThat(hgnc.getTerminology()).isEqualTo("hgnc");
     assertThat(hgnc.getMetadata().getUiLabel()).isEqualTo("HGNC: HUGO Gene Nomenclature Committee");
-    assertThat(hgnc.getName()).isEqualTo("HGNC 202209");
+    assertThat(hgnc.getName()).isEqualTo("HGNC: HUGO Gene Nomenclature Committee 202209");
     assertThat(hgnc.getDescription()).isNotEmpty();
 
     assertThat(hgnc.getMetadata().getLoader()).isEqualTo("rdf");
