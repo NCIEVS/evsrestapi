2f30476bfaacd963fe05d3f116a78f570ea238da
bcarlsenca <bcarlsen@westcoastinformatics.com>
2023-09-30 01:18:15 +0100

    Add government shutdown banner to swagger

--- src/main/java/gov/nih/nci/evs/api/configuration/OpenAPIDefinition.java
+++ src/main/java/gov/nih/nci/evs/api/configuration/OpenAPIDefinition.java
@@ -11,7 +11,13 @@ import io.swagger.v3.oas.annotations.servers.Server;
  */
 @OpenAPIDefinition(info = @Info(title = "NCI EVS Rest API", version = "1.7.2.RELEASE",
     termsOfService = "https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/ThesaurusTermsofUse.htm",
-    description = "Endpoints to support searching, metadata, and content retrieval for EVS terminologies. "
+    description = "<p><b>Government Funding Lapse</b></p>"
+        + "<p>Because of a lapse in government funding, the information on this website may not be up to date, transactions submitted via the website may not be processed, and the agency may not be able to respond to inquiries until appropriations are enacted. \r\n"
+        + "The NIH Clinical Center (the research hospital of NIH) is open. For more details about its operating status, please visit "
+        + "<a href=\"https://www.cc.nih.gov/\">cc.nih.gov</a>. \r\n"
+        + "Updates regarding government operating status and resumption of normal operations can be found at "
+        + "<a href=\"https://www.opm.gov/\">OPM.gov</a>.</p>"
+        + "Endpoints to support searching, metadata, and content retrieval for EVS terminologies. "
         + "To learn more about how to interact with this api, see the "
         + "<a href=\"https://github.com/NCIEVS/evsrestapi-client-SDK\">Github evsrestapi-client-SDK project.</a>",
     contact = @Contact(name = "NCI EVS", email = "NCIAppSupport@nih.gov")), servers = {
