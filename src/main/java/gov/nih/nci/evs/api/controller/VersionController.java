059724cd6ef3bfa58053b01bb94a2889728657d9
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2023-10-31 15:05:43 -0700

    Separate out swagger files so we can control the display.  Then add code for the banner (#218)

--- src/main/java/gov/nih/nci/evs/api/controller/VersionController.java
+++ src/main/java/gov/nih/nci/evs/api/controller/VersionController.java
@@ -49,7 +49,7 @@ public class VersionController extends BaseController {
             + "To learn more about how to interact with this api, see the  "
             + "<a href='https://github.com/NCIEVS/evsrestapi-client-SDK' "
             + "target='_blank'>Github evsrestapi-client-SDK project</a>.<br/><br/>");
-    homePageData.setVersion("1.8.0.RELEASE");
+    homePageData.setVersion("1.8.1.RELEASE");
     return homePageData;
   }
 
