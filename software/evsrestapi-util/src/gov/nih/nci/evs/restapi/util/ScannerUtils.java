ac90cd9dfa9484170557250d7efbd822b187b01e
kimongl <kim.ong@nih.gov>
2024-01-03 10:51:16 -0500

    Updated EVSStatistics.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ScannerUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ScannerUtils.java
@@ -99,6 +99,8 @@ public class ScannerUtils {
 	}
 
     public static String xml2Delimited(String line) {
+		//System.out.println(line);
+
 		line = line.trim();
 		if (line.length() == 0) return null;
 		int n = line.indexOf(" ");
@@ -194,6 +196,7 @@ public class ScannerUtils {
 		Vector v = new Vector();
 		Vector w = new Vector();
 		String id = null;
+		boolean include = false;
 		for (int i=0; i<owl_vec.size(); i++) {
 			String line = (String) owl_vec.elementAt(i);
 			//////////////////////////////////
@@ -205,29 +208,63 @@ public class ScannerUtils {
 				line = line + " " + nextLine;
 			}
 			//////////////////////////////////
+
+			//System.out.println("START " + line);
+			if (line.indexOf("<owl:Axiom>") != -1) {
+                include = false;
+			}
+
 			if (isOpenClass(line)) {
 				w = new Vector();
 				id = extractIdFromOpenClassLine(line);
+				//System.out.println("id: " + id);
+				include = true;
+
 			} else if (isCloseClass(line)) {
 				if (w != null && w.size() > 0) {
 					v.addAll(prefixId(id, w));
 				}
+				include = false;
+				//System.out.println("isCloseClass: " + line);
+
 			} else {
-				String t = xml2Delimited(line);
-				if (t != null) {
-					if (propertyCode != null) {
-						String tagKey = getTagKey(t);
-						if (tagKey.compareTo(propertyCode) == 0) {
+				try {
+					/*
+					String t = xml2Delimited(line);
+					if (t != null) {
+						if (propertyCode != null) {
+							String tagKey = getTagKey(t);
+							if (tagKey.compareTo(propertyCode) == 0) {
+								w.add(t);
+							}
+						} else {
+							//System.out.println("t: " + t);
 							w.add(t);
 						}
-					} else {
-						w.add(t);
 					}
+					*/
+					String t = getPropertyNameAndValue(line);
+					if (t != null && include) {
+						if (propertyCode != null) {
+							String tagKey = getTagKey(t);
+							if (tagKey.compareTo(propertyCode) == 0) {
+								w.add(t);
+							}
+						} else {
+							w.add(t);
+						}
+					}
+				} catch (Exception ex) {
+					//System.out.println(line);
+					ex.printStackTrace();
 				}
 			}
 		}
-		return v;
+		//removePrefix
+		//return v;
+		return removePrefix(ns, v);
 	}
+
 
 	public static Vector prefixId(String id, Vector v) {
 		Vector w = new Vector();
@@ -313,7 +350,7 @@ public class ScannerUtils {
 				}
 			}
 		}
-		return v;
+		return removePrefix(ns, v);
 	}
 
     public static void test(String[] args) {
@@ -332,12 +369,197 @@ public class ScannerUtils {
         System.out.println(line + "\n" + removePrefix(ns, t));
 	}
 
-    public static void main(String[] args) {
-        String owlfile = args[0];
-        Vector owl_vec = Utils.readFile(owlfile);
-        Vector w = ScannerUtils.extractProperties(owl_vec);
-        Utils.saveToFile("property_" + owlfile, w);
-        w = ScannerUtils.extractAxioms(owl_vec);
-        Utils.saveToFile("axiom_" + owlfile, w);
+    public static String getPropertyNameAndValue(String line) {
+       line = line.trim();
+       int n1 = line.indexOf(">");
+       if (n1 != -1) {
+		   String open_tag = line.substring(1, n1);
+		   int n2 = line.lastIndexOf("</");
+		   if (n2 != -1) {
+			   String close_tag = line.substring(n2+2, line.length()-1);
+			   if (open_tag.compareTo(close_tag) == 0) {
+				   String t = xml2Delimited(line);
+				   return t;
+			   }
+		   }
+	   }
+	   return null;
+	}
+
+	public static Vector extractAssociations(Vector owl_vec) {
+		String associationCode = null;
+		return extractAssociations(owl_vec, associationCode);
+	}
+
+
+	public static Vector extractAssociations(Vector owl_vec, String associationCode) {
+		Vector v = new Vector();
+		Vector w = new Vector();
+		String id = null;
+		boolean include = false;
+		for (int i=0; i<owl_vec.size(); i++) {
+			String line = (String) owl_vec.elementAt(i);
+			//////////////////////////////////
+			if (line.indexOf("General axioms") != -1) break;
+			while (!line.endsWith(">") && i < owl_vec.size()-1) {
+				i++;
+				String nextLine = (String) owl_vec.elementAt(i);
+				nextLine = nextLine.trim();
+				line = line + " " + nextLine;
+			}
+			//////////////////////////////////
+
+			//System.out.println("START " + line);
+			if (line.indexOf("<owl:Axiom>") != -1) {
+                include = false;
+			}
+
+			if (isOpenClass(line)) {
+				w = new Vector();
+				id = extractIdFromOpenClassLine(line);
+				include = true;
+
+			} else if (isCloseClass(line)) {
+				if (w != null && w.size() > 0) {
+					v.addAll(prefixId(id, w));
+				}
+				include = false;
+			} else {
+				try {
+					String t = getAssociationNameAndValue(line);
+					if (t != null && include) {
+						if (associationCode != null) {
+							int n = t.indexOf("|");
+							String assoCode = t.substring(0, n);
+							if (assoCode.compareTo(associationCode) == 0) {
+								w.add(t);
+							}
+						} else {
+							int n = t.indexOf("|");
+							String assoCode = t.substring(0, n);
+							if (assoCode.indexOf(":") == -1) {
+								w.add(t);
+							}
+						}
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		return removePrefix(ns, v);
+	}
+
+    public static String getAssociationNameAndValue(String line) {
+       line = line.trim();
+       String t = null;
+       String rdf_resource = "rdf:resource";
+       int n1 = line.indexOf(rdf_resource);
+       if (n1 != -1) {
+		   String associationName = line.substring(1, n1-1);
+		   int n2 = line.lastIndexOf("/>");
+		   if (n2 != -1) {
+			   String associationValue = line.substring(n1+rdf_resource.length()+2, line.length()-3);
+			   t = associationName + "|" + associationValue;
+		   }
+	   }
+	   return t;
+	}
+
+	public static Vector extractAllProperties(Vector owl_vec) {
+		Vector v1 = extractProperties(owl_vec);
+		Vector v2 = extractAssociations(owl_vec);
+		v1.addAll(v2);
+		return v1;
+	}
+
+	public static boolean isInteger(String str) {
+		try {
+			Integer.parseInt(str);
+			return true;
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+	}
+
+	public static boolean isConceptCode(String s) {
+		if (s.length() < 2) return false;
+		char c = s.charAt(0);
+		if (c != 'C') return false;
+
+		String t = s.substring(1, s.length());
+		return isInteger(t);
+	}
+
+/*
+C99999|P90|PERCUTANEOUS CORONARY INTERVENTION (PCI) FOR HIGH RISK NON-ST ELEVATION MYOCARDIAL INFARCTION OR UNSTABLE ANGINA|P383$PT|P384$CDISC
+C99999|P90|Percutaneous Coronary Intervention for High Risk Non-ST Elevation Myocardial Infarction or Unstable Angina|P383$PT|P384$NCI
+*/
+/*
+    public static HashMap getPropertyQualifier2CountMap(Vector owl_vec) {
+        Vector w = ScannerUtils.extractAxioms(owl_vec);
+        HashMap hmap = new HashMap();
+        for (int i=0; i<w.size(); i++) {
+			String line = (String) w.elementAt(i);
+			Vector u = StringUtils.parseData(line, '|');
+			String s0 = (String) u.elementAt(0);
+			if (isConceptCode(s0)) {
+				String propertyCode = (String) u.elementAt(1);
+				if (u.size() > 3) {
+					for (int j=3; j<u.size(); j++) {
+						String q = (String) u.elementAt(j);
+						Vector u2 = StringUtils.parseData(q, '$');
+						String q_code = (String) u2.elementAt(0);
+						int count = 0;
+						if (hmap.containsKey(propertyCode + "|" + q_code)) {
+							Integer int_obj = (Integer) hmap.get(propertyCode + "|" + q_code);
+							count = int_obj.intValue();
+						}
+						count++;
+						hmap.put(propertyCode + "|" + q_code, new Integer(count));
+					}
+			    }
+			}
+		}
+		return hmap;
+	}
+/*
+    public static HashMap createPropertyQualifier2CountMap(Vector w) {
+        HashMap hmap = new HashMap();
+        for (int i=0; i<w.size(); i++) {
+			String line = (String) w.elementAt(i);
+			Vector u = StringUtils.parseData(line, '|');
+			String s0 = (String) u.elementAt(0);
+			if (isConceptCode(s0)) {
+				String propertyCode = (String) u.elementAt(1);
+				if (u.size() > 3) {
+					for (int j=3; j<u.size(); j++) {
+						String q = (String) u.elementAt(j);
+						Vector u2 = StringUtils.parseData(q, '$');
+						String q_code = (String) u2.elementAt(0);
+						int count = 0;
+						if (hmap.containsKey(propertyCode + "|" + q_code)) {
+							Integer int_obj = (Integer) hmap.get(propertyCode + "|" + q_code);
+							count = int_obj.intValue();
+						}
+						count++;
+						hmap.put(propertyCode + "|" + q_code, new Integer(count));
+					}
+			    }
+			}
+		}
+		return hmap;
+	}
+*/
+
+
+    public static void dumpPropertyQualifier2CountMap(HashMap hmap) {
+		Iterator it = hmap.keySet().iterator();
+		while (it.hasNext()) {
+			String key = (String) it.next();
+			Integer int_obj = (Integer) hmap.get(key);
+			System.out.println(key + " " + int_obj.intValue());
+		}
 	}
 }
+
