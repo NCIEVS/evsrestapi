c2c911589f5c9c1297ada9a8cea8704dc974b70a
Alicia G. <32443614+gaaliciA1990@users.noreply.github.com>
2024-01-10 12:14:39 -0500

    Removed all circular dependencies. Fixed Caching issue

--- src/main/java/gov/nih/nci/evs/api/service/SparqlQueryManagerServiceImpl.java
+++ src/main/java/gov/nih/nci/evs/api/service/SparqlQueryManagerServiceImpl.java
@@ -112,6 +112,9 @@ public class SparqlQueryManagerServiceImpl implements SparqlQueryManagerService 
   /** The sparql query cache service */
   @Autowired SparqlQueryCacheService sparqlQueryCacheService;
 
+  /** The sparql query cache service */
+  @Autowired SparqlQueryCacheService sparqlQueryCacheService;
+
   /** The utils. */
   @Autowired TerminologyUtils utils;
 
@@ -152,6 +155,7 @@ public class SparqlQueryManagerServiceImpl implements SparqlQueryManagerService 
   public String getQueryURL() {
     return stardogProperties.getQueryUrl();
   }
+  // Here check the qualified form as well as the URI
 
   // Here check the qualified form as well as the URI
 
