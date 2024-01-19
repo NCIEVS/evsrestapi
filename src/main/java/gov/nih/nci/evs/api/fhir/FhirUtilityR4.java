451fb0b01d859e077fa6d9822ca2f5470c56dc5f
peter-va <pvanaus@ucdavis.edu>
2024-01-18 16:46:31 -0800

    add uri

--- src/main/java/gov/nih/nci/evs/api/fhir/FhirUtilityR4.java
+++ src/main/java/gov/nih/nci/evs/api/fhir/FhirUtilityR4.java
@@ -167,6 +167,7 @@ public final class FhirUtilityR4 {
     group.setTargetElement(new UriType(getUri(mapset.getProperties().stream()
         .filter(m -> m.getType().equals("targetTerminology")).findFirst().get().getValue())));
     cm.addGroup(group);
+    cm.setUrl(group.getSourceElement().asStringValue() + "?fhris_cm=$" + mapset.getCode());
     return cm;
   }
 
