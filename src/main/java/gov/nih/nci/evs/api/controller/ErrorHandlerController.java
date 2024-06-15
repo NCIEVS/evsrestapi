3178da344510b874f582a0c40740e63afddd3bdf
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2024-06-14 16:36:08 -1000

    EVSRESTAPI-459: fix errors add swagger (#269)

--- src/main/java/gov/nih/nci/evs/api/controller/ErrorHandlerController.java
+++ src/main/java/gov/nih/nci/evs/api/controller/ErrorHandlerController.java
@@ -31,7 +31,7 @@ public class ErrorHandlerController implements ErrorController {
   /** The error attributes. */
   private ErrorAttributes errorAttributes;
 
-  /** The error attribute options to include stack trace */
+  /** The error attribute options to include stack trace. */
   private ErrorAttributeOptions options =
       ErrorAttributeOptions.defaults().including(ErrorAttributeOptions.Include.STACK_TRACE);
 
@@ -116,6 +116,20 @@ public class ErrorHandlerController implements ErrorController {
       HttpServletRequest request, ErrorAttributeOptions options) {
     WebRequest webRequest = new ServletWebRequest(request);
     Map<String, Object> body = errorAttributes.getErrorAttributes(webRequest, options);
+
+    // Hack to convert "trace" to message
+    // TODO: fix this, and probably override what getErrorAttributes is doing to include the
+    // "reason" from the response status exception
+    if (body.containsKey("trace")) {
+      final String message =
+          body.get("trace")
+              .toString()
+              .split("\n")[0]
+              .replaceFirst(".*?\"", "")
+              .replaceFirst("[\"\r]+$", "");
+      body.put("message", message);
+    }
+
     if (body.containsKey("message")) {
       try {
         final String message = body.get("message").toString();
@@ -141,6 +155,11 @@ public class ErrorHandlerController implements ErrorController {
     return body;
   }
 
+  /**
+   * Returns the error path.
+   *
+   * @return the error path
+   */
   /* see superclass */
   public String getErrorPath() {
     return "/error";
