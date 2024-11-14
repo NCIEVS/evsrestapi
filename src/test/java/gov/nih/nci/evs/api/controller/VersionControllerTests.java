5bd63965a69a41a83b3ba2b6f000fa8326f74139
peter-va <pvanaus@ucdavis.edu>
2024-11-13 21:06:51 -0800

    fix spotless

--- src/test/java/gov/nih/nci/evs/api/controller/VersionControllerTests.java
+++ src/test/java/gov/nih/nci/evs/api/controller/VersionControllerTests.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.nih.nci.evs.api.properties.TestProperties;
+import gov.nih.nci.evs.api.support.ApplicationVersion;
+import gov.nih.nci.evs.api.util.ConceptUtils;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,12 +32,6 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import gov.nih.nci.evs.api.properties.TestProperties;
-import gov.nih.nci.evs.api.support.ApplicationVersion;
-import gov.nih.nci.evs.api.util.ConceptUtils;
 
 /** Integration tests for VersionController. */
 @ExtendWith(SpringExtension.class)
@@ -627,7 +624,7 @@ public class VersionControllerTests {
 
     return null;
   }
-  
+
   /**
    * Returns the builds the gradle version.
    *
@@ -638,21 +635,26 @@ public class VersionControllerTests {
   public void testGetSwaggerUIVersion() throws Exception {
     // Load properties from pom.properties
     Properties properties = new Properties();
-    properties.load(Files.newInputStream(Paths.get("src/main/resources/META-INF/maven/org.webjars/swagger-ui/pom.properties"))); 
-    
+    properties.load(
+        Files.newInputStream(
+            Paths.get("src/main/resources/META-INF/maven/org.webjars/swagger-ui/pom.properties")));
+
     // Get swagger-ui version
     String swaggerUiVersion = properties.getProperty("version");
-    
+
     if (swaggerUiVersion == null || swaggerUiVersion.isEmpty()) {
-        throw new IllegalStateException("Swagger UI version not found in properties");
+      throw new IllegalStateException("Swagger UI version not found in properties");
     }
 
     // Get path to version
     String path = "src/main/resources/META-INF/resources/webjars/swagger-ui/" + swaggerUiVersion;
-    
+
     // Check if the path exists and the versions match
     assertThat(Files.exists(Paths.get(path)))
-    .withFailMessage("The Swagger UI version might not match the folder structure: expected version %s at path %s", swaggerUiVersion, path)
-    .isTrue();
+        .withFailMessage(
+            "The Swagger UI version might not match the folder structure: expected version %s at"
+                + " path %s",
+            swaggerUiVersion, path)
+        .isTrue();
   }
 }
