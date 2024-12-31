4aadd8afa63982ccfea3b125c38863152ea878d6
peter-va <pvanaus@ucdavis.edu>
2024-12-30 18:13:58 -0800

    check duplicates

--- src/main/java/gov/nih/nci/evs/api/service/AbstractStardogLoadServiceImpl.java
+++ src/main/java/gov/nih/nci/evs/api/service/AbstractStardogLoadServiceImpl.java
@@ -1,5 +1,25 @@
 package gov.nih.nci.evs.api.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.nih.nci.evs.api.model.Association;
+import gov.nih.nci.evs.api.model.AssociationEntry;
+import gov.nih.nci.evs.api.model.Concept;
+import gov.nih.nci.evs.api.model.ConceptMinimal;
+import gov.nih.nci.evs.api.model.History;
+import gov.nih.nci.evs.api.model.IncludeParam;
+import gov.nih.nci.evs.api.model.Mapping;
+import gov.nih.nci.evs.api.model.Property;
+import gov.nih.nci.evs.api.model.Qualifier;
+import gov.nih.nci.evs.api.model.Terminology;
+import gov.nih.nci.evs.api.model.TerminologyMetadata;
+import gov.nih.nci.evs.api.properties.StardogProperties;
+import gov.nih.nci.evs.api.support.es.ElasticLoadConfig;
+import gov.nih.nci.evs.api.support.es.ElasticObject;
+import gov.nih.nci.evs.api.util.ConceptUtils;
+import gov.nih.nci.evs.api.util.HierarchyUtils;
+import gov.nih.nci.evs.api.util.MainTypeHierarchy;
+import gov.nih.nci.evs.api.util.TerminologyUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -24,7 +44,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -38,28 +57,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.NoSuchIndexException;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.util.CollectionUtils;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import gov.nih.nci.evs.api.model.Association;
-import gov.nih.nci.evs.api.model.AssociationEntry;
-import gov.nih.nci.evs.api.model.Concept;
-import gov.nih.nci.evs.api.model.ConceptMinimal;
-import gov.nih.nci.evs.api.model.History;
-import gov.nih.nci.evs.api.model.IncludeParam;
-import gov.nih.nci.evs.api.model.Mapping;
-import gov.nih.nci.evs.api.model.Property;
-import gov.nih.nci.evs.api.model.Qualifier;
-import gov.nih.nci.evs.api.model.Terminology;
-import gov.nih.nci.evs.api.model.TerminologyMetadata;
-import gov.nih.nci.evs.api.properties.StardogProperties;
-import gov.nih.nci.evs.api.support.es.ElasticLoadConfig;
-import gov.nih.nci.evs.api.support.es.ElasticObject;
-import gov.nih.nci.evs.api.util.ConceptUtils;
-import gov.nih.nci.evs.api.util.HierarchyUtils;
-import gov.nih.nci.evs.api.util.MainTypeHierarchy;
-import gov.nih.nci.evs.api.util.TerminologyUtils;
 
 /** The implementation for {@link ElasticLoadService}. */
 // @Service
@@ -549,8 +546,19 @@ public abstract class AbstractStardogLoadServiceImpl extends BaseLoaderService {
       computed.setValue("This is computed by the existing subset relationship");
       computed.setType("computed");
       assoc.getQualifiers().add(computed);
-      subsetToAddTo.getAssociations().add(assoc);
-      higherLevelSubset.getChildren().add(subsetToAddTo);
+      boolean exists = false;
+      for (Association a : subsetToAddTo.getAssociations()) {
+        if (a.getRelatedCode().equals(assoc.getRelatedCode())
+            && a.getType().equals(assoc.getType())) {
+          exists = true;
+          break;
+        }
+      }
+
+      if (!exists) {
+        subsetToAddTo.getAssociations().add(assoc);
+        higherLevelSubset.getChildren().add(subsetToAddTo);
+      }
 
       boolean isFirstRow = true;
       for (Row row : sheet) {
@@ -578,7 +586,18 @@ public abstract class AbstractStardogLoadServiceImpl extends BaseLoaderService {
         computed.setValue("This is computed by the existing subset relationship");
         computed.setType("computed");
         assoc.getQualifiers().add(computed);
-        conceptToAdd.getAssociations().add(assoc);
+        exists = false;
+        for (Association a : conceptToAdd.getAssociations()) {
+          if (a.getRelatedCode().equals(assoc.getRelatedCode())
+              && a.getType().equals(assoc.getType())) {
+            exists = true;
+            break;
+          }
+        }
+
+        if (!exists) {
+          conceptToAdd.getAssociations().add(assoc);
+        }
       }
     }
     return internalDataSet;
