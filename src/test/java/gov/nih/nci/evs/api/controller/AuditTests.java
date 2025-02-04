c83561f0786790eaeac0df971a63fa191ac2ffb6
peter-va <pvanaus@ucdavis.edu>
2025-02-04 13:56:29 -0800

    spotless formatting

--- src/test/java/gov/nih/nci/evs/api/controller/AuditTests.java
+++ src/test/java/gov/nih/nci/evs/api/controller/AuditTests.java
@@ -6,8 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.nih.nci.evs.api.model.Audit;
+import gov.nih.nci.evs.api.model.SearchCriteria;
+import gov.nih.nci.evs.api.model.Terminology;
+import gov.nih.nci.evs.api.properties.TestProperties;
+import gov.nih.nci.evs.api.service.ElasticQueryServiceImpl;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -18,15 +24,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import gov.nih.nci.evs.api.model.Audit;
-import gov.nih.nci.evs.api.model.SearchCriteria;
-import gov.nih.nci.evs.api.model.Terminology;
-import gov.nih.nci.evs.api.properties.TestProperties;
-import gov.nih.nci.evs.api.service.ElasticQueryServiceImpl;
 
 /** audit tests. */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -41,15 +38,13 @@ public class AuditTests {
 
   /** The test properties. */
   @Autowired TestProperties testProperties;
-  
+
   /** The elastic query service. */
   @Autowired private ElasticQueryServiceImpl elasticQueryService;
 
   /** Sets the up. */
   @BeforeEach
-  public void setUp() {
-
-  }
+  public void setUp() {}
 
   /**
    * Test get all audits and check against terminologies.
@@ -61,7 +56,7 @@ public class AuditTests {
     List<Audit> audits = elasticQueryService.getAllAudits(new SearchCriteria());
     assertNotNull(audits);
     assertThat(audits.size()).isGreaterThan(0);
-    
+
     // get terminologies to check against
     String url = "/api/v1/metadata/terminologies";
     log.info("Testing url - " + url);
