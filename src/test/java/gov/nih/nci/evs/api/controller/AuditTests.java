155f9bb9e62b126982e7f8794e9b78e0194c2876
peter-va <pvanaus@ucdavis.edu>
2025-02-04 13:48:42 -0800

    feedback, remove controller and change tests

--- src/test/java/gov/nih/nci/evs/api/controller/AuditTests.java
+++ src/test/java/gov/nih/nci/evs/api/controller/AuditTests.java
@@ -6,56 +6,49 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import gov.nih.nci.evs.api.model.Audit;
-import gov.nih.nci.evs.api.model.Terminology;
-import gov.nih.nci.evs.api.properties.TestProperties;
 import java.util.List;
-import java.util.stream.IntStream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.nih.nci.evs.api.model.Audit;
+import gov.nih.nci.evs.api.model.SearchCriteria;
+import gov.nih.nci.evs.api.model.Terminology;
+import gov.nih.nci.evs.api.properties.TestProperties;
+import gov.nih.nci.evs.api.service.ElasticQueryServiceImpl;
+
 /** audit tests. */
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class AuditControllerTests {
+public class AuditTests {
 
   /** The logger. */
-  private static final Logger log = LoggerFactory.getLogger(AuditControllerTests.class);
+  private static final Logger log = LoggerFactory.getLogger(AuditTests.class);
 
   /** The mvc. */
   @Autowired private MockMvc mvc;
 
   /** The test properties. */
   @Autowired TestProperties testProperties;
-
-  /** The object mapper. */
-  private ObjectMapper objectMapper;
-
-  /** The base url. */
-  private String baseUrl = "";
+  
+  /** The elastic query service. */
+  @Autowired private ElasticQueryServiceImpl elasticQueryService;
 
   /** Sets the up. */
   @BeforeEach
   public void setUp() {
 
-    objectMapper = new ObjectMapper();
-    JacksonTester.initFields(this, objectMapper);
-
-    baseUrl = "/api/v1/audit";
   }
 
   /**
@@ -65,36 +58,16 @@ public class AuditControllerTests {
    */
   @Test
   public void testGetAllAudits() throws Exception {
-    String url = baseUrl + "?sort=terminology&ascending=false";
-    log.info("Testing url - " + url);
-
-    // Act
-    MvcResult result = mvc.perform(get(url)).andExpect(status().isOk()).andReturn();
-    String content = result.getResponse().getContentAsString();
-    log.info(" content = " + content);
-    List<Audit> audits =
-        objectMapper.readValue(
-            content,
-            new TypeReference<List<Audit>>() {
-              // n/a
-            });
+    List<Audit> audits = elasticQueryService.getAllAudits(new SearchCriteria());
     assertNotNull(audits);
     assertThat(audits.size()).isGreaterThan(0);
-    // check for correct sorting (reverse terminology order)
-    assertThat(
-            IntStream.range(1, audits.size())
-                .allMatch(
-                    i ->
-                        audits.get(i - 1).getTerminology().compareTo(audits.get(i).getTerminology())
-                            >= 0))
-        .isTrue();
-
+    
     // get terminologies to check against
-    url = "/api/v1/metadata/terminologies";
+    String url = "/api/v1/metadata/terminologies";
     log.info("Testing url - " + url);
 
-    result = mvc.perform(get(url)).andExpect(status().isOk()).andReturn();
-    content = result.getResponse().getContentAsString();
+    MvcResult result = mvc.perform(get(url)).andExpect(status().isOk()).andReturn();
+    String content = result.getResponse().getContentAsString();
     log.info("  content = " + content);
     List<Terminology> terminologies =
         new ObjectMapper()
@@ -124,19 +97,7 @@ public class AuditControllerTests {
    */
   @Test
   public void testGetAuditsByTerminology() throws Exception {
-    String url = baseUrl + "/terminology/ncit";
-    log.info("Testing url - " + url);
-
-    // Act
-    MvcResult result = mvc.perform(get(url)).andExpect(status().isOk()).andReturn();
-    String content = result.getResponse().getContentAsString();
-    log.info(" content = " + content);
-    List<Audit> audits =
-        objectMapper.readValue(
-            content,
-            new TypeReference<List<Audit>>() {
-              // n/a
-            });
+    List<Audit> audits = elasticQueryService.getAuditsByTerminology("ncit");
     assertNotNull(audits);
     // two ncit terminologies, two ncit audit entries
     assertEquals(2, audits.size());
@@ -155,19 +116,9 @@ public class AuditControllerTests {
    */
   @Test
   public void testGetAuditsByType() throws Exception {
-    String url = baseUrl + "/type/reindex";
-    log.info("Testing url - " + url);
 
     // Act
-    MvcResult result = mvc.perform(get(url)).andExpect(status().isOk()).andReturn();
-    String content = result.getResponse().getContentAsString();
-    log.info(" content = " + content);
-    List<Audit> audits =
-        objectMapper.readValue(
-            content,
-            new TypeReference<List<Audit>>() {
-              // n/a
-            });
+    List<Audit> audits = elasticQueryService.getAuditsByType("reindex");
     assertNotNull(audits);
     // all entries are REINDEX
     assertThat(audits.stream().allMatch(audit -> audit.getType().equals("reindex"))).isTrue();
