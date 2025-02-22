cecbf3aeb051130b1306463879ba8eea20fbc283
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2025-02-22 20:29:24 +0000

    docker fixes

--- docker/fuseki/entrypoint.sh
+++ docker/fuseki/entrypoint.sh
@@ -5,16 +5,17 @@ export JAVA_OPTIONS="-Xmx8000M"
 export DEBUG=1
 ./fuseki start
 
-# Wait for the server to start
-echo "Wait 10 seconds to create databases"
-sleep 10
-
 # These steps are needed to ensure re-mounting the volume works properly
-# Create the NCIT2 dataset
+# Wait until server is up
+echo "Waiting for Fuseki to finish starting up..."
+until $(curl --output /dev/null --silent --head --fail http://localhost:3030); do
+  sleep 1s
+done
+
+# Create the NCIT2 and CTRP datasets
 curl -s -g -X POST -d "dbName=NCIT2&dbType=tdb2" "http://localhost:3030/\$/datasets"
-# Create the CTRP dataset
 curl -s -g -X POST -d "dbName=CTRP&dbType=tdb2" "http://localhost:3030/\$/datasets"
 
 # Keep the container running
-echo "do a tail -f /dev/null to keep the container running"
-tail -f /dev/null
+echo "keep the container by doing a tail on the logs"
+tail -f run/logs/stderrout.log
