9823051a86261967d30662e808e126f58170d07c
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2025-06-04 11:24:32 -0700

    fix option

--- src/main/bin/reindex.sh
+++ src/main/bin/reindex.sh
@@ -483,8 +483,8 @@ for x in `cat /tmp/y.$$.txt`; do
         # regardless of whether there was new data
         echo "    RECONCILE $term stale indexes and update flags"
         export EVS_SERVER_PORT="8083"
-        echo "java --add-opens=java.base/java.io=ALL-UNNAMED $local -Xm4096M -XX:+ExitOnOutOfMemoryError -jar $jar --terminology ${term} --skipConcepts --skipMetadata > /tmp/x.$$.log 2>&1"
-        java --add-opens=java.base/java.io=ALL-UNNAMED $local -Xm4096M -XX:+ExitOnOutOfMemoryError -jar $jar --terminology ${term} --skipConcepts --skipMetadata > /tmp/x.$$.log 2>&1 
+        echo "java --add-opens=java.base/java.io=ALL-UNNAMED $local -Xmx4096M -XX:+ExitOnOutOfMemoryError -jar $jar --terminology ${term} --skipConcepts --skipMetadata > /tmp/x.$$.log 2>&1"
+        java --add-opens=java.base/java.io=ALL-UNNAMED $local -Xmx4096M -XX:+ExitOnOutOfMemoryError -jar $jar --terminology ${term} --skipConcepts --skipMetadata > /tmp/x.$$.log 2>&1 
         if [[ $? -ne 0 ]]; then
             cat /tmp/x.$$.log | sed 's/^/    /'
             echo "ERROR: unexpected error building indexes"
@@ -567,7 +567,7 @@ done
 # Reconcile mappings after loading terminologies
 export EVS_SERVER_PORT="8083"
 echo "    Generate mapping indexes"
-echo "      java --add-opens=java.base/java.io=ALL-UNNAMED $local -Xm4096M -jar $jar --terminology mapping"
+echo "      java --add-opens=java.base/java.io=ALL-UNNAMED $local -Xmx4096M -jar $jar --terminology mapping"
 java --add-opens=java.base/java.io=ALL-UNNAMED $local -XX:+ExitOnOutOfMemoryError -Xmx4096M -jar $jar --terminology mapping
 if [[ $? -ne 0 ]]; then
     echo "ERROR: unexpected error building mapping indexes"
