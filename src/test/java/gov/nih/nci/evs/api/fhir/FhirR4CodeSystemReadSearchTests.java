7ed98eab3b7ff26a1c7ec135bd10370a0b2c6f30
shapirod2 <dshapiro@westcoastinformatics.com>
2025-07-03 09:53:26 -0700

    EVSRESTAPI-558: spotless apply

--- src/test/java/gov/nih/nci/evs/api/fhir/FhirR4CodeSystemReadSearchTests.java
+++ src/test/java/gov/nih/nci/evs/api/fhir/FhirR4CodeSystemReadSearchTests.java
@@ -24,7 +24,6 @@ import org.hl7.fhir.r4.model.OperationOutcome;
 import org.hl7.fhir.r4.model.OperationOutcome.OperationOutcomeIssueComponent;
 import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.ResourceType;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
