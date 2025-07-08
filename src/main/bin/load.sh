5997eab61c96f13756969767b4584e08cc0a3195
peter-va <pvanaus@ucdavis.edu>
2025-07-07 21:22:44 -0700

    fix load canmed

--- src/main/bin/load.sh
+++ src/main/bin/load.sh
@@ -185,7 +185,7 @@ load_data(){
     load_terminology_data NCIT2 http://ChEBI_monthly ChEBI/chebi_213.owl
     load_terminology_data NCIT2 http://UmlsSemNet UmlsSemNet/umlssemnet.owl
     load_terminology_data NCIT2 http://MEDRT MED-RT/MEDRT.2025-06-02.owl
-    load_terminology_data NCIT2 http://Canmed CanMed/CANMED.2025-06.owl
+    load_terminology_data NCIT2 http://Canmed CanMed/CANMED.202506.owl
     load_terminology_data NCIT2 http://CTCAE CTCAE/ctcae5.owl
     load_terminology_data NCIT2 http://DUO_monthly DUO/duo_Feb21.owl
     load_terminology_data NCIT2 http://DUO_monthly DUO/iao_Dec20.owl
