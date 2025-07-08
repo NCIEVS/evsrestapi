51ce6b353f537f311ca017dd6b8061efc8097187
peter-va <pvanaus@ucdavis.edu>
2025-07-07 21:22:34 -0700

    fix canmed devreset

--- src/main/bin/devreset.sh
+++ src/main/bin/devreset.sh
@@ -297,7 +297,7 @@ load_data(){
     load_terminology_data NCIT2 http://HGNC_monthly HGNC/HGNC.202507.owl
     load_terminology_data NCIT2 http://ChEBI_monthly ChEBI/chebi_213.owl
     load_terminology_data NCIT2 http://UmlsSemNet UmlsSemNet/umlssemnet.owl
-    load_terminology_data NCIT2 http://Canmed CanMed/CANMED.2025-06.owl
+    load_terminology_data NCIT2 http://Canmed CanMed/CANMED.202506.owl
     load_terminology_data NCIT2 http://MEDRT MED-RT/MEDRT.2025-06-02.owl
     load_terminology_data NCIT2 http://CTCAE CTCAE/ctcae5.owl
     load_terminology_data NCIT2 http://DUO_monthly DUO/duo_Feb21.owl
