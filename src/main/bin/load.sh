5e1161768aa398b9727c68c31bdd4954b2c27a74
peter-va <pvanaus@ucdavis.edu>
2025-07-08 21:40:10 -0700

    edit filename for chebi

--- src/main/bin/load.sh
+++ src/main/bin/load.sh
@@ -90,8 +90,8 @@ fi
 
 # Check ChEBI monthly
 echo "    check ChEBI monthly"
-if [[ ! -e "$dir/ChEBI/chebi_213.owl" ]]; then
-    echo "ERROR: unexpectedly missing ChEBI/chebi_213.owl file"
+if [[ ! -e "$dir/ChEBI/chebi_241.owl" ]]; then
+    echo "ERROR: unexpectedly missing ChEBI/chebi_241.owl file"
     exit 1
 fi
 
@@ -182,7 +182,7 @@ load_data(){
     load_terminology_data NCIT2 http://NCI_T_monthly ThesaurusInferred_monthly.owl
     load_terminology_data NCIT2 http://GO_monthly GO/GO.20250601.owl
     load_terminology_data NCIT2 http://HGNC_monthly HGNC/HGNC.202507.owl
-    load_terminology_data NCIT2 http://ChEBI_monthly ChEBI/chebi_213.owl
+    load_terminology_data NCIT2 http://ChEBI_monthly ChEBI/chebi_241.owl
     load_terminology_data NCIT2 http://UmlsSemNet UmlsSemNet/umlssemnet.owl
     load_terminology_data NCIT2 http://MEDRT MED-RT/MEDRT.2025-06-02.owl
     load_terminology_data NCIT2 http://Canmed CanMed/CANMED.202506.owl
