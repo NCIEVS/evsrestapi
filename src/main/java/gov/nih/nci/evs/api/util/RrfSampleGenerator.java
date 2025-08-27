e56c3a968940bbcd264a4f15b0dd8fe9a1b61a54
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2025-08-26 18:35:49 -0700

    add comment

--- src/main/java/gov/nih/nci/evs/api/util/RrfSampleGenerator.java
+++ src/main/java/gov/nih/nci/evs/api/util/RrfSampleGenerator.java
@@ -250,6 +250,8 @@ public class RrfSampleGenerator {
       // Parameterize this!
       final File outputDir = new File(inputPath, "/RRF-subset/");
       copier.setActiveOnly(false);
+      // TODO: we probably should pass in codesabs here and keep atoms
+      // that match them and then other downstream data matching those AUIs only.
       copier.copyFiles(new File(inputPath), outputDir, terminologies, cuis);
 
       logger.info("Done ...");
