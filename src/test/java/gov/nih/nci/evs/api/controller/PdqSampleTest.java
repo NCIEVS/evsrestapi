b3a76b527fd5c3a8c1eae12cff6e7cbb9fd8e09f
peter-va <pvanaus@ucdavis.edu>
2025-08-29 15:28:34 -0700

    more fixed terminologies

---
+++ src/test/java/gov/nih/nci/evs/api/controller/PdqSampleTest.java
@@ -0,0 +1,71 @@
+package gov.nih.nci.evs.api.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.nih.nci.evs.api.model.Concept;
+
+/** NCIt samples test. */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PdqSampleTest extends SampleTest {
+
+  /** The logger. */
+  private static final Logger log = LoggerFactory.getLogger(PdqSampleTest.class);
+
+  /** The test mvc. Used by CheckZzz methods to avoid taking as a param. */
+  @Autowired private MockMvc testMvc;
+
+  /**
+   * Setup class.
+   *
+   * @throws Exception the exception
+   */
+  @BeforeAll
+  public static void setupClass() throws Exception {
+    loadSamples("pdq", "src/test/resources/samples/pdq-samples.txt");
+  }
+
+  /**
+   * Test concept active status.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testActive() throws Exception {
+
+    String url = null;
+    MvcResult result = null;
+    String content = null;
+    Concept concept = null;
+
+    // Test active
+    url = "/api/v1/concept/pdq/100001";
+    log.info("Testing url - " + url);
+    result = testMvc.perform(get(url)).andExpect(status().isOk()).andReturn();
+    content = result.getResponse().getContentAsString();
+    log.info(" content = " + content);
+    concept = new ObjectMapper().readValue(content, Concept.class);
+    assertThat(concept).isNotNull();
+    assertThat(concept.getCode()).isEqualTo("100001");
+    assertThat(concept.getTerminology()).isEqualTo("pdq");
+    assertThat(concept.getActive()).isTrue();
+  }
+}
