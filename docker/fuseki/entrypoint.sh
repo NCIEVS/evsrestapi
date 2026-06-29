be002e20c7678b6651505940b3df74ac543f66c5
akuppusamy-wci <akuppusamy@westcoastinformatics.com>
2026-06-28 22:01:34 -0700

    Update Dockerfile and entrypoint script for Jena 6.1.0 and Java 25

--- docker/fuseki/entrypoint.sh
+++ docker/fuseki/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Start the fuseki-server
+# Start the fuseki-server (foreground runner, backgrounded here)
 export JAVA_OPTIONS="-Xmx8000M"
 export DEBUG=1
-./fuseki start
+./fuseki-server &
+FUSEKI_PID=$!
 
 # These steps are needed to ensure re-mounting the volume works properly
 # Wait until server is up
@@ -16,6 +17,6 @@ done
 curl -s -g -X POST -d "dbName=NCIT2&dbType=tdb2" "http://localhost:3030/\$/datasets"
 curl -s -g -X POST -d "dbName=CTRP&dbType=tdb2" "http://localhost:3030/\$/datasets"
 
-# Keep the container running
-echo "keep the container by doing a tail on the logs"
-tail -f run/logs/stderrout.log
+# Keep the container running by waiting on the fuseki process
+echo "keep the container by waiting on the fuseki-server process"
+wait $FUSEKI_PID
